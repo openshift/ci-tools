c04f62ae6375f5c1b4dbc9a4a7b98e07683c0e6e
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2018-09-17 17:22:25 +0000

    Return all validation errors

--- pkg/api/config.go
+++ pkg/api/config.go
@@ -1,23 +1,28 @@
 package api
 
-import "errors"
+import (
+	"errors"
+
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
+)
 
 // Validate validates config
 func (config *ReleaseBuildConfiguration) Validate() error {
+	errs := []error{}
 	if config.Tests != nil {
 		for _, test := range config.Tests {
 			if test.As == "images" {
-				return errors.New("test should not be called 'images' because it gets confused with '[images]' target")
+				errs = append(errs, errors.New("test should not be called 'images' because it gets confused with '[images]' target"))
 			}
 		}
 	}
 	buildRootImage := config.InputConfiguration.BuildRootImage
 	if buildRootImage != nil {
 		if buildRootImage.ProjectImageBuild != nil && buildRootImage.ImageStreamTagReference != nil {
-			return errors.New("both git_source_image and image_stream_tag cannot be set for the build_root")
+			errs = append(errs, errors.New("both git_source_image and image_stream_tag cannot be set for the build_root"))
 		} else if buildRootImage.ProjectImageBuild == nil && buildRootImage.ImageStreamTagReference == nil {
-			return errors.New("you have to specify either git_source_image or image_stream_tag for the build_root")
+			errs = append(errs, errors.New("you have to specify either git_source_image or image_stream_tag for the build_root"))
 		}
 	}
-	return nil
+	return kerrors.NewAggregate(errs)
 }

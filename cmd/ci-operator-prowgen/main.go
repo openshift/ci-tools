0f9e719d243945e5276b309d7e436e1c01db3e46
Steve Kuznetsov <skuznets@redhat.com>
2018-09-24 10:10:21 -0700

    Always pull the images for tests
    We use rolling tags for images used for tests, so we need to ensure that
    those are always pulled down before tests are run or rollouts to update
    the images underlying tests are nondeterministic.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- cmd/ci-operator-prowgen/main.go
+++ cmd/ci-operator-prowgen/main.go
@@ -92,10 +92,11 @@ func generatePodSpec(org, repo, configFile, target string, additionalArgs ...str
 		ServiceAccountName: "ci-operator",
 		Containers: []kubeapi.Container{
 			{
-				Image:   "ci-operator:latest",
-				Command: []string{"ci-operator"},
-				Args:    append([]string{"--artifact-dir=$(ARTIFACTS)", fmt.Sprintf("--target=%s", target)}, additionalArgs...),
-				Env:     []kubeapi.EnvVar{{Name: "CONFIG_SPEC", ValueFrom: &configMapKeyRef}},
+				Image:           "ci-operator:latest",
+				ImagePullPolicy: kubeapi.PullAlways,
+				Command:         []string{"ci-operator"},
+				Args:            append([]string{"--artifact-dir=$(ARTIFACTS)", fmt.Sprintf("--target=%s", target)}, additionalArgs...),
+				Env:             []kubeapi.EnvVar{{Name: "CONFIG_SPEC", ValueFrom: &configMapKeyRef}},
 				Resources: kubeapi.ResourceRequirements{
 					Requests: kubeapi.ResourceList{"cpu": *resource.NewMilliQuantity(10, resource.DecimalSI)},
 					Limits:   kubeapi.ResourceList{"cpu": *resource.NewMilliQuantity(500, resource.DecimalSI)},

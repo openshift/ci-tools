1247c7a5e00e9edc55eb9992da880da77f00ec26
Steve Kuznetsov <skuznets@redhat.com>
2019-01-27 07:49:27 -0800

    Update the location of the clonerefs file
    Upstream changed the name of their `bazel` workspace, so the output
    location of the clonerefs app binary moved.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -303,7 +303,7 @@ func stepConfigsForBuild(config *api.ReleaseBuildConfiguration, jobSpec *api.Job
 			Name:      "clonerefs",
 			Tag:       "latest",
 		},
-		ClonerefsPath: "/app/prow/cmd/clonerefs/app.binary.runfiles/test_infra/prow/cmd/clonerefs/linux_amd64_pure_stripped/app.binary",
+		ClonerefsPath: "/app/prow/cmd/clonerefs/app.binary.runfiles/io_k8s_test_infra/prow/cmd/clonerefs/linux_amd64_pure_stripped/app.binary",
 	}})
 
 	if len(config.BinaryBuildCommands) > 0 {

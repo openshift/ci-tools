318ac8429a59aa2c9fc4a22bcec6df32164207e9
Petr Muller <muller@redhat.com>
2019-03-04 16:15:24 +0100

    rehearse: optimize git manipulation when loading configs
    Previously we loaded all three CI config fragments (Prow config,
    ci-operator config, templates) separately. Because we need to load the
    configs from two different revisions of the release repo, all three
    loading methods handled the "check out master revision, load config,
    check out tested revision" procedure themselves, which resulted in the
    whole operation being performed three times unnecessarily. This was
    optimized so that we separate loading of all config from the git
    operation.
    
    A little tricky area is the error handling and propagation. The errors
    occured during config loading itself are not propagated via error values
    but by the appropriate field of the config having a nil value. This is
    because we need granular information about which configs loaded
    correctly and which not, so the tool can decide a right course of
    action. The decisions are as follows:
    
    - Exit when any Prow config version loading failed, because we need to
      compare both versions to compute which jobs should be rehearsed.
    - Exit when PR version of templates or ci-operator config loading
      failed, because we need these versions to provide them to rehearsed
      jobs.
    - Do not compare templates when master version of templates fails to
      load.
    - Do not compare ci-operator config when master version of these configs
      fails to load.

---
+++ pkg/config/template.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	templateapi "github.com/openshift/api/template/v1"
+	templatescheme "github.com/openshift/client-go/template/clientset/versioned/scheme"
+)
+
+type CiTemplates map[string]*templateapi.Template
+
+func getTemplates(templatePath string) (CiTemplates, error) {
+	templates := make(map[string]*templateapi.Template)
+	err := filepath.Walk(templatePath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("prevent panic by handling failure accessing a path %q: %v", path, err)
+		}
+
+		if isYAML(path, info) {
+			contents, err := ioutil.ReadFile(path)
+			if err != nil {
+				return fmt.Errorf("could not read file %s for template: %v", path, err)
+			}
+
+			if obj, _, err := templatescheme.Codecs.UniversalDeserializer().Decode(contents, nil, nil); err == nil {
+				if template, ok := obj.(*templateapi.Template); ok {
+					if len(template.Name) == 0 {
+						template.Name = filepath.Base(path)
+						template.Name = strings.TrimSuffix(template.Name, filepath.Ext(template.Name))
+					}
+					templates[template.Name] = template
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error walking the path %q: %v", templatePath, err)
+	}
+	return templates, nil
+}
+
+func isYAML(file string, info os.FileInfo) bool {
+	return !info.IsDir() && (filepath.Ext(file) == ".yaml" || filepath.Ext(file) == ".yml")
+}

e47368c3396b326fa87c2dd9487bab67c3ce053c
Nikolaos Moraitis <nmoraiti@redhat.com>
2019-03-08 15:46:37 +0100

    add a cleanup temporary configmaps step

--- cmd/pj-rehearse/main.go
+++ cmd/pj-rehearse/main.go
@@ -100,6 +100,15 @@ func gracefulExit(suppressFailures bool, message string) {
 	os.Exit(1)
 }
 
+func gracefulExitWithConfigMapCleanup(suppressFailures bool, message string, cmManager *config.TemplateCMManager) {
+	if cmManager != nil {
+		if err := cmManager.CleanupCMTemplates(); err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to cleanup temporary configmaps")
+		}
+	}
+	gracefulExit(suppressFailures, message)
+}
+
 func main() {
 	o := gatherOptions()
 	err := validateOptions(o)
@@ -191,13 +200,13 @@ func main() {
 	cmManager := config.NewTemplateCMManager(cmClient, prNumber, logger, changedTemplates)
 	if err := cmManager.CreateCMTemplates(); err != nil {
 		logger.WithError(err).Error("couldn't create template configMap")
-		gracefulExit(o.noFail, "")
+		gracefulExitWithConfigMapCleanup(o.noFail, "", cmManager)
 	}
 
 	pjclient, err := rehearse.NewProwJobClient(clusterConfig, namespace, o.dryRun)
 	if err != nil {
 		logger.WithError(err).Error("could not create a ProwJob client")
-		gracefulExit(o.noFail, misconfigurationOutput)
+		gracefulExitWithConfigMapCleanup(o.noFail, misconfigurationOutput, cmManager)
 	}
 
 	debugLogger := logrus.New()
@@ -208,7 +217,7 @@ func main() {
 			debugLogger.Out = f
 		} else {
 			logger.WithError(err).Error("could not open debug log file")
-			gracefulExit(o.noFail, "")
+			gracefulExitWithConfigMapCleanup(o.noFail, "", cmManager)
 		}
 	}
 	loggers := rehearse.Loggers{Job: logger, Debug: debugLogger.WithField(prowgithub.PrLogField, prNumber)}
@@ -219,25 +228,29 @@ func main() {
 	rehearsals := rehearse.ConfigureRehearsalJobs(toRehearse, prConfig.CiOperator, prNumber, loggers, o.allowVolumes, changedTemplates)
 	if len(rehearsals) == 0 {
 		logger.Info("no jobs to rehearse have been found")
-		gracefulExit(true, "")
+		gracefulExitWithConfigMapCleanup(true, "", cmManager)
 	} else if len(rehearsals) > o.rehearsalLimit {
 		jobCountFields := logrus.Fields{
 			"rehearsal-threshold": o.rehearsalLimit,
 			"rehearsal-jobs":      len(rehearsals),
 		}
 		logger.WithFields(jobCountFields).Info("Would rehearse too many jobs, will not proceed")
-		gracefulExit(true, "")
+		gracefulExitWithConfigMapCleanup(true, "", cmManager)
 	}
 
 	executor := rehearse.NewExecutor(rehearsals, prNumber, o.releaseRepoPath, jobSpec.Refs, o.dryRun, loggers, pjclient)
 	success, err := executor.ExecuteJobs()
 	if err != nil {
 		logger.WithError(err).Error("Failed to rehearse jobs")
-		gracefulExit(o.noFail, rehearseFailureOutput)
+		gracefulExitWithConfigMapCleanup(o.noFail, rehearseFailureOutput, cmManager)
 	}
 	if !success {
 		logger.Error("Some jobs failed their rehearsal runs")
-		gracefulExit(o.noFail, jobsFailureOutput)
+		gracefulExitWithConfigMapCleanup(o.noFail, jobsFailureOutput, cmManager)
 	}
 	logger.Info("All jobs were rehearsed successfully")
+
+	if err := cmManager.CleanupCMTemplates(); err != nil {
+		logger.WithError(err).Error("Failed to cleanup temporary configmaps")
+	}
 }

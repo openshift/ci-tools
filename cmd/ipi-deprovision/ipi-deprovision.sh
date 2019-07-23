706c6477d14d2c2d99383e74dfe9ab1eee283dab
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-07-23 17:38:58 -0400

    Add comments on ipi-deprovision.sh

--- cmd/ipi-deprovision/ipi-deprovision.sh
+++ cmd/ipi-deprovision/ipi-deprovision.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 if [ -z "${AWS_SHARED_CREDENTIALS_FILE}" ]; then echo "ENV VAR AWS_SHARED_CREDENTIALS_FILE is not set"; exit 1; fi
 
 ### expirationDate is always with timezone +00:00, eg, "2019-07-23T22:35+0000"
 MY_DATE=$(TZ=":Africa/Abidjan" date '+%Y-%m-%d')
+### TODO: ask @abhinav.dahiya the right list of regions we should focus on
+### here is only for test
 declare -a regions=("us-east-1" "us-east-2" "us-west-1")
 
 handle_cluster () {
   local cluster_name;
   cluster_name=$1
   echo "handling cluster: ${cluster_name} ..."
+  ### TODO: will test hiveutil later
+  #./bin/hiveutil aws-tag-deprovision "${cluster_name}=owned"
 }
 
 for r in "${regions[@]}"

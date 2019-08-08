2724a482578d95c978694a554b88aa37d35a5dd7
Nikolaos Moraitis <nmoraiti@redhat.com>
2019-08-09 00:33:36 +0200

    add tests for git source

---
+++ pkg/steps/git_source_test.go
@@ -0,0 +1,169 @@
+package steps
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/util/diff"
+	prowapi "k8s.io/test-infra/prow/apis/prowjobs/v1"
+)
+
+func TestDetermineRefsWorkdir(t *testing.T) {
+	testCases := []struct {
+		testName  string
+		refs      *prowapi.Refs
+		extraRefs []prowapi.Refs
+		expected  *prowapi.Refs
+	}{
+		{
+			testName:  "no workdir, nil Refs/ExtraRefs, expect nil",
+			refs:      nil,
+			extraRefs: nil,
+			expected:  nil,
+		},
+		{
+			testName: "no workdir, expect Refs",
+			refs: &prowapi.Refs{
+				Org:     "org",
+				Repo:    "repo",
+				BaseRef: "branch",
+			},
+			extraRefs: []prowapi.Refs{
+				{
+					Org:     "testOrg1",
+					Repo:    "testRepo1",
+					BaseRef: "master",
+				},
+				{
+
+					Org:     "testOrg2",
+					Repo:    "testRepo2",
+					BaseRef: "master",
+				},
+				{
+
+					Org:     "testOrg3",
+					Repo:    "testRepo3",
+					BaseRef: "master",
+				},
+			},
+			expected: &prowapi.Refs{
+				Org:     "org",
+				Repo:    "repo",
+				BaseRef: "branch",
+			},
+		},
+		{
+			testName: "no workdir, nil Refs, expect extraRefs[0]",
+			refs:     nil,
+			extraRefs: []prowapi.Refs{
+				{
+					Org:     "testOrg1",
+					Repo:    "testRepo1",
+					BaseRef: "master",
+				},
+				{
+
+					Org:     "testOrg2",
+					Repo:    "testRepo2",
+					BaseRef: "master",
+				},
+				{
+
+					Org:     "testOrg3",
+					Repo:    "testRepo3",
+					BaseRef: "master",
+				},
+			},
+			expected: &prowapi.Refs{
+				Org:     "testOrg1",
+				Repo:    "testRepo1",
+				BaseRef: "master",
+			},
+		},
+
+		{
+			testName: "workdir, expect extraRefs with workdir",
+			refs: &prowapi.Refs{
+				Org:     "org",
+				Repo:    "repo",
+				BaseRef: "branch",
+			},
+			extraRefs: []prowapi.Refs{
+				{
+					Org:     "testOrg1",
+					Repo:    "testRepo1",
+					BaseRef: "master",
+				},
+				{
+
+					Org:     "testOrg2",
+					Repo:    "testRepo2",
+					BaseRef: "master",
+				},
+				{
+					Org:     "org-with-workdir",
+					Repo:    "repo-with-workdir",
+					BaseRef: "branch",
+					WorkDir: true,
+				},
+				{
+
+					Org:     "testOrg3",
+					Repo:    "testRepo3",
+					BaseRef: "master",
+				},
+			},
+			expected: &prowapi.Refs{
+				Org:     "org-with-workdir",
+				Repo:    "repo-with-workdir",
+				BaseRef: "branch",
+				WorkDir: true,
+			},
+		},
+		{
+			testName: "workdir, nil refs, expect extraRefs with workdir",
+			refs:     nil,
+			extraRefs: []prowapi.Refs{
+				{
+					Org:     "testOrg1",
+					Repo:    "testRepo1",
+					BaseRef: "master",
+				},
+				{
+
+					Org:     "testOrg2",
+					Repo:    "testRepo2",
+					BaseRef: "master",
+				},
+				{
+					Org:     "org-with-workdir",
+					Repo:    "repo-with-workdir",
+					BaseRef: "branch",
+					WorkDir: true,
+				},
+				{
+
+					Org:     "testOrg3",
+					Repo:    "testRepo3",
+					BaseRef: "master",
+				},
+			},
+			expected: &prowapi.Refs{
+				Org:     "org-with-workdir",
+				Repo:    "repo-with-workdir",
+				BaseRef: "branch",
+				WorkDir: true,
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			ref := determineRefsWorkdir(testCase.refs, testCase.extraRefs)
+			if !equality.Semantic.DeepEqual(ref, testCase.expected) {
+				t.Errorf("Refs are different than expected: %v", diff.ObjectReflectDiff(ref, testCase.expected))
+			}
+		})
+	}
+}

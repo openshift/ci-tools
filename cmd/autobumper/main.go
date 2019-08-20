4d1ca58936d209fb409d09d68782785182944474
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-08-20 09:32:47 -0400

    Start to implement autobumper

---
+++ cmd/autobumper/main.go
@@ -0,0 +1,7 @@
+// copied from go/src/k8s.io/test-infra/experiment/autobumper/main.go
+// originally based on: 3c673e642 (HEAD -> master, origin/master, origin/HEAD) Merge pull request #13978 from nilo19/t-qini-update_k8s_and_aks_engine_version_for_periodic_tests
+package main
+
+func main() {
+
+}

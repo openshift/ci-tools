0955fc5598ad9d1ea0da708528d38d43410938b5
Alex Pavel <apavel@redhat.com>
2019-09-13 11:02:01 -0700

    don't use globals

--- cmd/ci-operator-configresolver/main.go
+++ cmd/ci-operator-configresolver/main.go
@@ -18,68 +18,70 @@ import (
 	types "github.com/openshift/ci-tools/pkg/steps/types"
 )
 
-var (
+type options struct {
 	resolver   registry.Resolver
 	configPath string
 	regPath    string
-)
-
-func resolveConfig(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.WriteHeader(http.StatusNotImplemented)
-		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
-		return
-	}
-	org := r.URL.Query().Get("org")
-	if org == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("org query missing or incorrect"))
-		return
-	}
-	repo := r.URL.Query().Get("repo")
-	if repo == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("repo query missing or incorrect"))
-		return
-	}
-	branch := r.URL.Query().Get("branch")
-	if branch == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("branch query missing or incorrect"))
-		return
-	}
-	variant := r.URL.Query().Get("variant")
-	info := config.Info{
-		Org:     org,
-		Repo:    repo,
-		Branch:  branch,
-		Variant: variant,
-	}
-	info.Filename = filepath.Join(configPath, branch, info.Basename())
-
-	config, err := load.Config(info.Filename)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "could not load config: %v", err)
-		return
-	}
-	if err := config.Validate(info.Org, info.Repo); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "config validation failed: %v", err)
-		return
-	}
-	config.Tests, err = resolveConfigExternal(config.Tests)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "config resolution failed: %v", err)
-		return
-	}
-	jsonConfig, err := json.Marshal(config)
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonConfig)
 }
 
-func resolveConfigExternal(config []api.TestStepConfiguration) ([]api.TestStepConfiguration, error) {
+func resolveConfig(o *options) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusNotImplemented)
+			w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+			return
+		}
+		org := r.URL.Query().Get("org")
+		if org == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("org query missing or incorrect"))
+			return
+		}
+		repo := r.URL.Query().Get("repo")
+		if repo == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("repo query missing or incorrect"))
+			return
+		}
+		branch := r.URL.Query().Get("branch")
+		if branch == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("branch query missing or incorrect"))
+			return
+		}
+		variant := r.URL.Query().Get("variant")
+		info := config.Info{
+			Org:     org,
+			Repo:    repo,
+			Branch:  branch,
+			Variant: variant,
+		}
+		info.Filename = filepath.Join(o.configPath, branch, info.Basename())
+
+		config, err := load.Config(info.Filename)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "could not load config: %v", err)
+			return
+		}
+		if err := config.Validate(info.Org, info.Repo); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "config validation failed: %v", err)
+			return
+		}
+		config.Tests, err = resolveConfigExternal(config.Tests, o)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "config resolution failed: %v", err)
+			return
+		}
+		jsonConfig, err := json.Marshal(config)
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonConfig)
+	}
+}
+
+func resolveConfigExternal(config []api.TestStepConfiguration, o *options) ([]api.TestStepConfiguration, error) {
 	var result []api.TestStepConfiguration
 	for _, step := range config {
 		if step.MultiStageTestConfiguration == nil {
@@ -92,7 +94,7 @@ func resolveConfigExternal(config []api.TestStepConfiguration) ([]api.TestStepCo
 			ArtifactDir: step.ArtifactDir,
 			Secret:      step.Secret,
 		}
-		resolvedFlow, err := resolver.Resolve(*step.MultiStageTestConfiguration)
+		resolvedFlow, err := o.resolver.Resolve(*step.MultiStageTestConfiguration)
 		if err != nil {
 			return result, err
 		}
@@ -125,12 +127,12 @@ func internalStepsToAPISteps(steps []types.TestStep) []api.TestStep {
 	return newSteps
 }
 
-func reloadRegistry(path string) error {
-	references, chain, workflows, err := load.Registry(path)
+func reloadRegistry(o *options) error {
+	references, chain, workflows, err := load.Registry(o.regPath)
 	if err != nil {
 		return err
 	}
-	resolver = registry.NewResolver(references, chain, workflows)
+	o.resolver = registry.NewResolver(references, chain, workflows)
 	return nil
 }
 
@@ -150,10 +152,11 @@ func populateWatcher(watcher *fsnotify.Watcher, root string) error {
 }
 
 func main() {
-	flag.StringVar(&regPath, "registry", "", "Path to step registry")
-	flag.StringVar(&configPath, "config", "", "Path to config dirs")
+	opts := options{}
+	flag.StringVar(&opts.regPath, "registry", "", "Path to step registry")
+	flag.StringVar(&opts.configPath, "config", "", "Path to config dirs")
 	flag.Parse()
-	reloadRegistry(regPath)
+	reloadRegistry(&opts)
 
 	// watch for changes in config directories
 	watcher, err := fsnotify.NewWatcher()
@@ -166,12 +169,12 @@ func main() {
 			select {
 			case _ = <-watcher.Events:
 				log.Info("Reloading registry")
-				if err := reloadRegistry(regPath); err != nil {
+				if err := reloadRegistry(&opts); err != nil {
 					log.Fatalf("Failed to reload registry: %v", err)
 				}
 				log.Info("Registry successfully reloaded")
 				// add new files to be watched
-				if err := populateWatcher(watcher, regPath); err != nil {
+				if err := populateWatcher(watcher, opts.regPath); err != nil {
 					log.Fatalf("Failed to update fsnotify watchlist: %v", err)
 				}
 			case err := <-watcher.Errors:
@@ -179,8 +182,8 @@ func main() {
 			}
 		}
 	}()
-	populateWatcher(watcher, regPath)
+	populateWatcher(watcher, opts.regPath)
 
-	http.HandleFunc("/config", resolveConfig)
+	http.HandleFunc("/config", resolveConfig(&opts))
 	http.ListenAndServe(":8080", nil)
 }

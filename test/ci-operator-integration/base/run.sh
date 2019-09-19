bf5642c0dc81d94f502464326473e0fc4167a45d
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2019-09-19 08:28:26 +0000

    ci-operator int tests: minor fixes

--- test/ci-operator-integration/base/run.sh
+++ test/ci-operator-integration/base/run.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 WORKDIR="$( mktemp -d )"
 trap 'rm -rf "${WORKDIR}"' EXIT
 
-readonly TEST_ROOT="$( dirname "${BASH_SOURCE[0]}")"
+TEST_ROOT="$(dirname "${BASH_SOURCE[0]}")"
+readonly TEST_ROOT
 readonly TEST_CONFIG="${TEST_ROOT}/config/test-config.yaml"
 readonly TEST_TEMPLATE="${TEST_ROOT}/config/test-template.yaml"
 readonly TEST_NAMESPACE="testns"
@@ -16,13 +17,14 @@ readonly EXPECTED_WITH_TEMPLATE="${TEST_ROOT}/expected_files/expected_with_templ
 readonly DRY_RUN_JSON="${WORKDIR}/ci-op-dry.json"
 readonly DRY_RUN_WITH_TEMPLATE_JSON="${WORKDIR}/ci-op-template-dry.json"
 
-readonly ARTIFACT_DIR="${WORKDIR/artifacts}"
+readonly ARTIFACT_DIR="${WORKDIR}/artifacts"
 
 export JOB_SPEC='{"type":"presubmit","job":"pull-ci-openshift-release-master-ci-operator-integration","buildid":"0","prowjobid":"uuid","refs":{"org":"openshift","repo":"ci-tools","base_ref":"master","base_sha":"af8a90a2faf965eeda949dc1c607c48d3ffcda3e","pulls":[{"number":1234,"author":"droslean","sha":"538680dfd2f6cff3b3506c80ca182dcb0dd22a58"}]}}'
+# set by Prow
 unset BUILD_ID
 
 echo "[INFO] Running ci-operator in dry-mode..."
-if ! ci-operator --dry-run --determinize-output --namespace "${TEST_NAMESPACE}" --config "${TEST_CONFIG}" 2> "${WORKDIR}/ci-op-stderr.log" | jq -S . > "${DRY_RUN_JSON}"; then
+if ! ci-operator --dry-run --determinize-output --namespace "${TEST_NAMESPACE}" --config "${TEST_CONFIG}" 2> "${WORKDIR}/ci-op-stderr.log" | jq --sort-keys . > "${DRY_RUN_JSON}"; then
     echo "ERROR: ci-operator failed."
     cat "${WORKDIR}/ci-op-stderr.log"
     exit 1

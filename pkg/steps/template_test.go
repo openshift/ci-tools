78434060d757873075003b7edaa3bee85c549b8c
Nikolaos Moraitis <nmoraiti@redhat.com>
2019-09-20 12:43:38 +0200

    add unit tests for resources injection

--- pkg/steps/template_test.go
+++ pkg/steps/template_test.go
@@ -10,11 +10,13 @@ import (
 	"k8s.io/test-infra/prow/pod-utils/downwardapi"
 
 	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/resource"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/diff"
 
 	templateapi "github.com/openshift/api/template/v1"
+
 	"github.com/openshift/ci-tools/pkg/api"
 )
 
@@ -83,6 +85,7 @@ func TestOperateOnTemplatePods(t *testing.T) {
 	testCases := []struct {
 		testID       string
 		artifactsDir string
+		resources    api.ResourceConfiguration
 		template     *templateapi.Template
 		expected     *templateapi.Template
 	}{
@@ -230,15 +233,334 @@ func TestOperateOnTemplatePods(t *testing.T) {
 				},
 			},
 		},
+		{
+			testID: "resources defined in the config but the test container has existing resources, no changes expected",
+			resources: api.ResourceConfiguration{
+				"*": {
+					Requests: api.ResourceList{"cpu": "1", "memory": "2Gi"},
+					Limits:   api.ResourceList{"memory": "3Gi"},
+				},
+				"test-template": {
+					Requests: api.ResourceList{"cpu": "5", "memory": "10Gi"},
+					Limits:   api.ResourceList{"memory": "16Gi"},
+				},
+			},
+			template: &templateapi.Template{
+				TypeMeta:   meta.TypeMeta{Kind: "Template", APIVersion: "template.openshift.io/v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-template"},
+				Objects: []runtime.RawExtension{
+					func() runtime.RawExtension {
+						pod := &coreapi.Pod{
+							TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+							ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+							Spec: coreapi.PodSpec{
+								Containers: []coreapi.Container{
+									{
+										Name: "test",
+										Resources: coreapi.ResourceRequirements{
+											Requests: coreapi.ResourceList{
+												"cpu":    *resource.NewQuantity(3, resource.DecimalSI),
+												"memory": *resource.NewQuantity(8*1024*1024*1024, resource.BinarySI)},
+											Limits: coreapi.ResourceList{"memory": *resource.NewQuantity(10*1024*1024*1024, resource.BinarySI)},
+										},
+									},
+								}},
+						}
+						return runtime.RawExtension{
+							Raw:    []byte(runtime.EncodeOrDie(corev1Codec, pod)),
+							Object: pod.DeepCopyObject(),
+						}
+					}(),
+				},
+			},
+			expected: &templateapi.Template{
+				TypeMeta:   meta.TypeMeta{Kind: "Template", APIVersion: "template.openshift.io/v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-template"},
+				Objects: []runtime.RawExtension{
+					func() runtime.RawExtension {
+						pod := &coreapi.Pod{
+							TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+							ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+							Spec: coreapi.PodSpec{
+								Containers: []coreapi.Container{
+									{
+										Name: "test",
+										Resources: coreapi.ResourceRequirements{
+											Requests: coreapi.ResourceList{
+												"cpu":    *resource.NewQuantity(3, resource.DecimalSI),
+												"memory": *resource.NewQuantity(8*1024*1024*1024, resource.BinarySI)},
+											Limits: coreapi.ResourceList{"memory": *resource.NewQuantity(10*1024*1024*1024, resource.BinarySI)},
+										},
+									},
+								},
+							},
+						}
+						return runtime.RawExtension{
+							Raw:    []byte(runtime.EncodeOrDie(corev1Codec, pod)),
+							Object: pod.DeepCopyObject(),
+						}
+					}(),
+				},
+			},
+		},
+		{
+			testID: "resources defined in the config,no existing resources in the container, changes are expected",
+			resources: api.ResourceConfiguration{
+				"*": {
+					Requests: api.ResourceList{"cpu": "1", "memory": "2Gi"},
+					Limits:   api.ResourceList{"memory": "3Gi"},
+				},
+				"test-template": {
+					Requests: api.ResourceList{"cpu": "5", "memory": "10Gi"},
+					Limits:   api.ResourceList{"memory": "16Gi"},
+				},
+			},
+			template: &templateapi.Template{
+				TypeMeta:   meta.TypeMeta{Kind: "Template", APIVersion: "template.openshift.io/v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-template"},
+				Objects: []runtime.RawExtension{
+					func() runtime.RawExtension {
+						pod := &coreapi.Pod{
+							TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+							ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+							Spec: coreapi.PodSpec{
+								Containers: []coreapi.Container{{Name: "test"}}},
+						}
+						return runtime.RawExtension{
+							Raw:    []byte(runtime.EncodeOrDie(corev1Codec, pod)),
+							Object: pod.DeepCopyObject(),
+						}
+					}(),
+				},
+			},
+			expected: &templateapi.Template{
+				TypeMeta:   meta.TypeMeta{Kind: "Template", APIVersion: "template.openshift.io/v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-template"},
+				Objects: []runtime.RawExtension{
+					func() runtime.RawExtension {
+						pod := &coreapi.Pod{
+							TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+							ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+							Spec: coreapi.PodSpec{
+								Containers: []coreapi.Container{
+									{
+										Name: "test",
+										Resources: coreapi.ResourceRequirements{
+											Requests: coreapi.ResourceList{
+												"cpu":    *resource.NewQuantity(5, resource.DecimalSI),
+												"memory": *resource.NewQuantity(10*1024*1024*1024, resource.BinarySI)},
+											Limits: coreapi.ResourceList{"memory": *resource.NewQuantity(16*1024*1024*1024, resource.BinarySI)},
+										},
+									},
+								},
+							},
+						}
+						return runtime.RawExtension{
+							Raw:    []byte(runtime.EncodeOrDie(corev1Codec, pod)),
+							Object: pod.DeepCopyObject(),
+						}
+					}(),
+				},
+			},
+		},
+		{
+			testID: "only default resources defined in the config,no existing resources in the container, changes are expected",
+			resources: api.ResourceConfiguration{
+				"*": {
+					Requests: api.ResourceList{"cpu": "1", "memory": "2Gi"},
+					Limits:   api.ResourceList{"memory": "3Gi"},
+				},
+			},
+			template: &templateapi.Template{
+				TypeMeta:   meta.TypeMeta{Kind: "Template", APIVersion: "template.openshift.io/v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-template"},
+				Objects: []runtime.RawExtension{
+					func() runtime.RawExtension {
+						pod := &coreapi.Pod{
+							TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+							ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+							Spec: coreapi.PodSpec{
+								Containers: []coreapi.Container{{Name: "test"}}},
+						}
+						return runtime.RawExtension{
+							Raw:    []byte(runtime.EncodeOrDie(corev1Codec, pod)),
+							Object: pod.DeepCopyObject(),
+						}
+					}(),
+				},
+			},
+			expected: &templateapi.Template{
+				TypeMeta:   meta.TypeMeta{Kind: "Template", APIVersion: "template.openshift.io/v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-template"},
+				Objects: []runtime.RawExtension{
+					func() runtime.RawExtension {
+						pod := &coreapi.Pod{
+							TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+							ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+							Spec: coreapi.PodSpec{
+								Containers: []coreapi.Container{
+									{
+										Name: "test",
+										Resources: coreapi.ResourceRequirements{
+											Requests: coreapi.ResourceList{
+												"cpu":    *resource.NewQuantity(1, resource.DecimalSI),
+												"memory": *resource.NewQuantity(2*1024*1024*1024, resource.BinarySI)},
+											Limits: coreapi.ResourceList{"memory": *resource.NewQuantity(3*1024*1024*1024, resource.BinarySI)},
+										},
+									},
+								},
+							},
+						}
+						return runtime.RawExtension{
+							Raw:    []byte(runtime.EncodeOrDie(corev1Codec, pod)),
+							Object: pod.DeepCopyObject(),
+						}
+					}(),
+				},
+			},
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.testID, func(t *testing.T) {
-			operateOnTemplatePods(tc.template, tc.artifactsDir)
+			operateOnTemplatePods(tc.template, tc.artifactsDir, tc.resources)
 			if !equality.Semantic.DeepEqual(tc.template, tc.expected) {
 				t.Fatal(diff.ObjectReflectDiff(tc.template, tc.expected))
 			}
 
+		})
+	}
+}
+
+func TestInjectResourcesToPod(t *testing.T) {
+	testTemplateName := "test-template"
+	testCases := []struct {
+		testID    string
+		resources api.ResourceConfiguration
+		pod       *coreapi.Pod
+		expected  *coreapi.Pod
+	}{
+		{
+			testID: "no resource requests are defined, container named 'test' exists, expect no changes",
+			pod: &coreapi.Pod{
+				TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+				Spec:       coreapi.PodSpec{Containers: []coreapi.Container{{Name: "test"}}},
+			},
+			expected: &coreapi.Pod{
+				TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+				Spec: coreapi.PodSpec{
+					Containers: []coreapi.Container{{Name: "test"}},
+				},
+			},
+		},
+		{
+			testID: "resource requests are defined, no container named 'test' exists, expect no changes",
+			resources: api.ResourceConfiguration{
+				testTemplateName: {
+					Requests: api.ResourceList{"cpu": "3", "memory": "8Gi"},
+					Limits:   api.ResourceList{"memory": "10Gi"},
+				},
+			},
+			pod: &coreapi.Pod{
+				TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+				Spec: coreapi.PodSpec{
+					Containers: []coreapi.Container{{Name: "no-test"}},
+				},
+			},
+			expected: &coreapi.Pod{
+				TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+				Spec: coreapi.PodSpec{
+					Containers: []coreapi.Container{{Name: "no-test"}},
+				},
+			},
+		},
+
+		{
+			testID: "both default and template's resource requests are defined, pod has container named 'test', and is changed",
+			resources: api.ResourceConfiguration{
+				"*": {
+					Requests: api.ResourceList{"cpu": "3", "memory": "8Gi"},
+					Limits:   api.ResourceList{"memory": "10Gi"},
+				},
+				"test-template": {
+					Requests: api.ResourceList{"cpu": "5", "memory": "10Gi"},
+					Limits:   api.ResourceList{"memory": "16Gi"},
+				},
+			},
+			pod: &coreapi.Pod{
+				TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+				Spec: coreapi.PodSpec{
+					Containers: []coreapi.Container{{Name: "test"}},
+				},
+			},
+			expected: &coreapi.Pod{
+				TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+				Spec: coreapi.PodSpec{
+					Containers: []coreapi.Container{
+						{
+							Name: "test",
+							Resources: coreapi.ResourceRequirements{
+								Requests: coreapi.ResourceList{
+									"cpu":    *resource.NewQuantity(5, resource.DecimalSI),
+									"memory": *resource.NewQuantity(10*1024*1024*1024, resource.BinarySI)},
+								Limits: coreapi.ResourceList{"memory": *resource.NewQuantity(16*1024*1024*1024, resource.BinarySI)},
+							},
+						},
+					},
+				},
+			},
+		},
+
+		{
+			testID: "only the default resource requests are defined, pod has container named 'test', and is changed",
+			resources: api.ResourceConfiguration{
+				"*": {
+					Requests: api.ResourceList{"cpu": "3", "memory": "8Gi"},
+					Limits:   api.ResourceList{"memory": "10Gi"},
+				},
+			},
+			pod: &coreapi.Pod{
+				TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+				Spec: coreapi.PodSpec{
+					Containers: []coreapi.Container{{Name: "test"}},
+				},
+			},
+			expected: &coreapi.Pod{
+				TypeMeta:   meta.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+				ObjectMeta: meta.ObjectMeta{Name: "test-pod"},
+				Spec: coreapi.PodSpec{
+					Containers: []coreapi.Container{
+						{
+							Name: "test",
+							Resources: coreapi.ResourceRequirements{
+								Requests: coreapi.ResourceList{
+									"cpu":    *resource.NewQuantity(3, resource.DecimalSI),
+									"memory": *resource.NewQuantity(8*1024*1024*1024, resource.BinarySI)},
+								Limits: coreapi.ResourceList{"memory": *resource.NewQuantity(10*1024*1024*1024, resource.BinarySI)},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testID, func(t *testing.T) {
+			pod := tc.pod
+			expectedPod := tc.expected
+
+			injectResourcesToPod(pod, testTemplateName, tc.resources)
+			if !equality.Semantic.DeepEqual(expectedPod, pod) {
+				t.Fatal(diff.ObjectDiff(expectedPod, pod))
+			}
 		})
 	}
 }

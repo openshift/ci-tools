0127db807e6363f709f2dd92b17f569c82a1a409
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2019-10-10 14:09:04 +0000

    Add a lease client

---
+++ pkg/lease/client.go
@@ -0,0 +1,113 @@
+package lease
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+
+	boskos "k8s.io/test-infra/boskos/client"
+	"k8s.io/test-infra/boskos/common"
+)
+
+const (
+	freeState   = "free"
+	leasedState = "leased"
+)
+
+type boskosClient interface {
+	Acquire(rtype, state, dest string) (*common.Resource, error)
+	UpdateOne(name, dest string, _ *common.UserData) error
+	ReleaseOne(name, dest string) error
+	ReleaseAll(dest string) error
+}
+
+// Client manages resource leases, acquiring, releasing, and keeping them
+// updated.
+type Client interface {
+	// Acquire leases a resource and returns the lease name.
+	// `cancel` is called if any subsequent updates to the lease fail.
+	Acquire(rtype string, cancel context.CancelFunc) (string, error)
+	// Heartbeat updates all leases. It calls the cancellation function of each
+	// lease it fails to update.
+	Heartbeat() error
+	// Release ends one lease by name.
+	Release(name string) error
+	// ReleaseAll ends all leases and returns the names of those that were
+	// successfully released.
+	ReleaseAll() ([]string, error)
+}
+
+// NewClient creates a client that leases resources with the specified owner.
+func NewClient(owner, url string) Client {
+	return newClient(boskos.NewClient(owner, url))
+}
+
+func newClient(boskos boskosClient) Client {
+	return &client{
+		boskos: boskos,
+		cancel: make(map[string]context.CancelFunc),
+	}
+}
+
+type client struct {
+	sync.RWMutex
+	boskos boskosClient
+	// cancel holds cancellation functions for steps that depend on leases
+	// being active; we must cancel these when we encounter errors to tie the
+	// lifetime of the downstream user routines to those of the leases they
+	// require
+	cancel map[string]context.CancelFunc
+}
+
+func (c *client) Acquire(rtype string, cancel context.CancelFunc) (string, error) {
+	r, err := c.boskos.Acquire(rtype, freeState, leasedState)
+	if err != nil {
+		return "", err
+	}
+	c.Lock()
+	c.cancel[r.Name] = cancel
+	c.Unlock()
+	return r.Name, nil
+}
+
+func (c *client) Heartbeat() error {
+	c.Lock()
+	defer c.Unlock()
+	var errs []error
+	for name, cancel := range c.cancel {
+		if err := c.boskos.UpdateOne(name, leasedState, nil); err != nil {
+			errs = append(errs, fmt.Errorf("failed to update lease %q: %v", name, err))
+			cancel()
+			delete(c.cancel, name)
+		}
+	}
+	return utilerrors.NewAggregate(errs)
+}
+
+func (c *client) Release(name string) error {
+	if err := c.boskos.ReleaseOne(name, freeState); err != nil {
+		return err
+	}
+	c.Lock()
+	delete(c.cancel, name)
+	c.Unlock()
+	return nil
+}
+
+func (c *client) ReleaseAll() ([]string, error) {
+	c.Lock()
+	defer c.Unlock()
+	var ret []string
+	var errs []error
+	for l := range c.cancel {
+		ret = append(ret, l)
+		if err := c.boskos.ReleaseOne(l, freeState); err != nil {
+			errs = append(errs, err)
+			continue
+		}
+		delete(c.cancel, l)
+	}
+	return ret, utilerrors.NewAggregate(errs)
+}
---
+++ pkg/lease/fake.go
@@ -0,0 +1,51 @@
+package lease
+
+import (
+	"fmt"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+
+	"k8s.io/test-infra/boskos/common"
+)
+
+type fakeClient struct {
+	owner    string
+	failures sets.String
+	calls    *[]string
+}
+
+func NewFakeClient(owner, url string, failures sets.String, calls *[]string) Client {
+	if calls == nil {
+		calls = &[]string{}
+	}
+	return newClient(&fakeClient{owner: owner, failures: failures, calls: calls})
+}
+
+func (c *fakeClient) addCall(call string, args ...string) error {
+	s := strings.Join(append([]string{call, c.owner}, args...), " ")
+	if c.calls != nil {
+		*c.calls = append(*c.calls, s)
+	}
+	if c.failures.Has(s) {
+		return fmt.Errorf("injected failure %q", call)
+	}
+	return nil
+}
+
+func (c *fakeClient) Acquire(rtype, state, dest string) (*common.Resource, error) {
+	err := c.addCall("acquire", rtype, state, dest)
+	return &common.Resource{Name: fmt.Sprintf("%s%d", rtype, len(*c.calls)-1)}, err
+}
+
+func (c *fakeClient) UpdateOne(name, dest string, _ *common.UserData) error {
+	return c.addCall("updateone", name, dest)
+}
+
+func (c *fakeClient) ReleaseOne(name, dest string) error {
+	return c.addCall("releaseone", name, dest)
+}
+
+func (c *fakeClient) ReleaseAll(dest string) error {
+	return c.addCall("releaseall", dest)
+}

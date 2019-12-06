e40ae6de97606e8e67a9249c75631d1b7e1f8374
Jeffrey Sica <jeef111x@gmail.com>
2019-12-06 09:16:22 -0500

    revert ide formatting

--- pkg/steps/pod_test.go
+++ pkg/steps/pod_test.go
@@ -3,7 +3,7 @@ package steps
 import (
 	"testing"
 
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/resource"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"

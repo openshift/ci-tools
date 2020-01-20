b89057c81f32674ef00155acf25a13ab7479a9c8
Hongkai Liu <hongkailiu@users.noreply.github.com>
2020-01-20 14:42:08 -0500

    Revert "fix retry around template instance ready check (in case it is called before the templates instance is created)"
    This reverts commit d92df0facdccf541f126d679750e214bd33c7c9d.

--- pkg/steps/template.go
+++ pkg/steps/template.go
@@ -382,23 +382,21 @@ func isPodCompleted(podClient coreclientset.PodInterface, name string) (bool, er
 }
 
 func waitForTemplateInstanceReady(templateClient templateclientset.TemplateInstanceInterface, instance *templateapi.TemplateInstance) (*templateapi.TemplateInstance, error) {
-	var actualErr error
-	err := wait.PollImmediate(2*time.Second, 10*time.Minute, func() (bool, error) {
-		instance, actualErr = templateClient.Get(instance.Name, meta.GetOptions{})
-		if actualErr != nil {
-			return false, nil
+	for {
+		ready, err := templateInstanceReady(instance)
+		if err != nil {
+			return nil, fmt.Errorf("could not determine if template instance was ready: %v", err)
 		}
-		ready := false
-		ready, actualErr = templateInstanceReady(instance)
 		if ready {
-			return true, nil
+			return instance, nil
 		}
-		return false, nil
-	})
-	if err == nil {
-		return instance, nil
+
+		time.Sleep(2 * time.Second)
+		instance, err = templateClient.Get(instance.Name, meta.GetOptions{})
+		if err != nil {
+			return nil, fmt.Errorf("unable to retrieve existing template instance: %v", err)
+		}
 	}
-	return nil, actualErr
 }
 
 func createOrRestartTemplateInstance(templateClient templateclientset.TemplateInstanceInterface, podClient coreclientset.PodInterface, instance *templateapi.TemplateInstance) (*templateapi.TemplateInstance, error) {
@@ -422,7 +420,6 @@ func createOrRestartTemplateInstance(templateClient templateclientset.TemplateIn
 func waitForCompletedTemplateInstanceDeletion(templateClient templateclientset.TemplateInstanceInterface, podClient coreclientset.PodInterface, name string) error {
 	instance, err := templateClient.Get(name, meta.GetOptions{})
 	if errors.IsNotFound(err) {
-		log.Printf("Template instance %s already deleted, do not need to wait any longer", name)
 		return nil
 	}
 
@@ -434,8 +431,6 @@ func waitForCompletedTemplateInstanceDeletion(templateClient templateclientset.T
 		Preconditions:     &meta.Preconditions{UID: &uid},
 	})
 	if errors.IsNotFound(err) {
-		log.Printf("After initial existence check, a delete of template %s and instance %s received a not found error ",
-			name, string(instance.UID))
 		return nil
 	}
 	if err != nil {

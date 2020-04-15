12f2dac3f3316ca71652301947bc7435053fa0e7
Alex Pavel <apavel@redhat.com>
2020-04-15 16:15:50 -0700

    prowgen test: remove test-secret var

--- cmd/ci-operator-prowgen/main_test.go
+++ cmd/ci-operator-prowgen/main_test.go
@@ -35,7 +35,6 @@ var unexportedFields = []cmp.Option{
 }
 
 func TestGeneratePodSpec(t *testing.T) {
-	testSecret := &ciop.Secret{Name: "secret-name", MountPath: "/usr/local/test-secret"}
 	tests := []struct {
 		description    string
 		info           *prowgenInfo
@@ -147,7 +146,7 @@ func TestGeneratePodSpec(t *testing.T) {
 		{
 			description:    "additional args and secret are included in podspec",
 			info:           &prowgenInfo{Info: config.Info{Org: "org", Repo: "repo", Branch: "branch"}},
-			secrets:        []*ciop.Secret{testSecret},
+			secrets:        []*ciop.Secret{{Name: "secret-name", MountPath: "/usr/local/test-secret"}},
 			targets:        []string{"target"},
 			additionalArgs: []string{"--promote", "--some=thing"},
 
@@ -166,7 +165,7 @@ func TestGeneratePodSpec(t *testing.T) {
 						"--promote",
 						"--some=thing",
 						"--target=target",
-						fmt.Sprintf("--secret-dir=/secrets/%s", testSecret.Name),
+						"--secret-dir=/secrets/secret-name",
 					},
 					Resources: kubeapi.ResourceRequirements{
 						Requests: kubeapi.ResourceList{"cpu": *resource.NewMilliQuantity(10, resource.DecimalSI)},
@@ -175,7 +174,7 @@ func TestGeneratePodSpec(t *testing.T) {
 						{Name: "sentry-dsn", MountPath: "/etc/sentry-dsn", ReadOnly: true},
 						{Name: "apici-ci-operator-credentials", ReadOnly: true, MountPath: "/etc/apici"},
 						{Name: "pull-secret", ReadOnly: true, MountPath: "/etc/pull-secret"},
-						{Name: testSecret.Name, MountPath: fmt.Sprintf("/secrets/%s", testSecret.Name), ReadOnly: true},
+						{Name: "secret-name", MountPath: "/secrets/secret-name", ReadOnly: true},
 					},
 				}},
 				Volumes: []kubeapi.Volume{
@@ -198,9 +197,9 @@ func TestGeneratePodSpec(t *testing.T) {
 						},
 					},
 					{
-						Name: testSecret.Name,
+						Name: "secret-name",
 						VolumeSource: kubeapi.VolumeSource{
-							Secret: &kubeapi.SecretVolumeSource{SecretName: testSecret.Name},
+							Secret: &kubeapi.SecretVolumeSource{SecretName: "secret-name"},
 						},
 					},
 				},

cea0bf36bb2e84ac99fc1de563ecc7c21c036791
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2020-04-29 16:11:14 -0400

    Actually run lint

--- hack/lint.sh
+++ hack/lint.sh
@@ -2,11 +2,10 @@
 
 set -euo pipefail
 
-echo "Checking gofmt"
-gofmt -s -l $(go list -f '{{ .Dir }}' ./... ) | grep ".*\.go"; if [ "$$?" = "0" ]; then gofmt -s -d $(shell go list -f '{{ .Dir }}' ./... ); exit 1; fi
-
-echo "Running go vet"
-go vet ./...
-
 echo "Running golangci-lint"
-golangci-lint run --disable-all --enable=unused,deadcode,gosimple ./...
+# CI has HOME set to '/' causing the linter to try and create a cache at /.cache for which
+# it doesn't have permissions.
+if [[ $HOME = '/' ]]; then
+  export HOME=/tmp
+fi
+golangci-lint run --disable-all --enable=unused,deadcode,gosimple,gofmt,govet ./...

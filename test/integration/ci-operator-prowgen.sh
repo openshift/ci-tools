1f4a710d71d0684c266968d5370ac184bb0fef78
Steve Kuznetsov <skuznets@redhat.com>
2020-05-14 20:09:20 -0700

    test: clean up prowgen test
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- test/integration/ci-operator-prowgen.sh
+++ test/integration/ci-operator-prowgen.sh
@@ -7,19 +7,16 @@ function cleanup() {
 }
 trap "cleanup" EXIT
 
-suite_dir="${OS_ROOT}/test/prowgen-integration/"
-input_config_dir="${suite_dir}/data/input/config"
-input_jobs_dir="${suite_dir}/data/input/jobs"
+suite_dir="${OS_ROOT}/test/prowgen-integration/data"
 actual="${BASETMPDIR}/jobs"
 mkdir -p "${actual}"
 # we need to seed this with the input data as we operate "in place"
-cp -a "${input_jobs_dir}/." "${actual}"
-expected="${suite_dir}/data/output/jobs"
+cp -a "${suite_dir}/input/jobs/." "${actual}"
 
 os::test::junit::declare_suite_start "integration/ci-operator-prowgen"
 # This test validates the ci-operator-prowgen tool
 
-os::cmd::expect_success "ci-operator-prowgen --from-dir ${input_config_dir} --to-dir ${actual}"
-os::integration::compare "${actual}" "${expected}"
+os::cmd::expect_success "ci-operator-prowgen --from-dir ${suite_dir}/input/config --to-dir ${actual}"
+os::integration::compare "${actual}" "${suite_dir}/output/jobs"
 
 os::test::junit::declare_suite_end

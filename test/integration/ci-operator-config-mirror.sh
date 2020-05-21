590aedbc4c61c6e5575ecf074af889a0dd8edda1
Nikolaos Moraitis <nmoraiti@redhat.com>
2020-05-21 16:25:57 +0200

    add integration test for both clean and non-clean runs
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

--- test/integration/ci-operator-config-mirror.sh
+++ test/integration/ci-operator-config-mirror.sh
@@ -2,15 +2,23 @@
 source "$(dirname "${BASH_SOURCE}")/../../hack/lib/init.sh"
 trap os::test::junit::reconcile_output EXIT
 
-suite_dir="${OS_ROOT}/test/integration/ci-operator-config-mirror/"
+suite_dir="${OS_ROOT}/test/integration/ci-operator-config-mirror"
 cp -r "${suite_dir}/input" "${BASETMPDIR}"
+cp -r "${suite_dir}/input-to-clean" "${BASETMPDIR}"
+
 actual="${BASETMPDIR}/input"
+actual_to_clean="${BASETMPDIR}/input-to-clean"
+
 expected="${suite_dir}/output"
 
 os::test::junit::declare_suite_start "integration/ci-operator-config-mirror"
 # This test validates the ci-operator-config-mirror tool
 
-os::cmd::expect_success "ci-operator-config-mirror --to-org super-priv --config-path ${actual} --whitelist-file ${suite_dir}/whitelist.yaml"
+os::cmd::expect_success "ci-operator-config-mirror --to-org super-priv --config-path ${actual} --clean=false --whitelist-file ${suite_dir}/whitelist.yaml"
 os::integration::compare "${actual}" "${expected}"
+
+os::cmd::expect_success "ci-operator-config-mirror --to-org super-priv --config-path ${actual_to_clean} --clean=true --whitelist-file ${suite_dir}/whitelist.yaml"
+os::integration::compare "${actual_to_clean}" "${expected}"
+
 
 os::test::junit::declare_suite_end

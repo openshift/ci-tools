f4f9ef6310f0e38f4433de2df4cfe2dfcce9b533
Steve Kuznetsov <skuznets@redhat.com>
2020-05-27 09:19:47 -0700

    ci-operator: change release step inputs and outputs
    We no longer try to do an image import to read the SHA of the input
    image when we have a literal image given to us by a user. This feature
    relied on a bug -- creating the import in an unrelated namespace -- and
    could not continue to exist. Furthermore, this feature is of limited
    utility: it will only come into use when the user provides a floating
    tag, not a full pull-spec, *and* two jobs with the same name and config
    race to run at the same time, but resolve that tag to different SHAs.
    
    We also no longer rely on the stable stream existing when we're running
    an inport from an environment variable. It's not clear why this was the
    case at all in the past, as the entire point of the input env vars was
    to override the default behavior and cause an import.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- pkg/steps/release/create_release.go
+++ pkg/steps/release/create_release.go
@@ -67,37 +67,7 @@ type assembleReleaseStep struct {
 
 func (s *assembleReleaseStep) Inputs(dry bool) (api.InputDefinition, error) {
 	if val, _ := s.params.Get(s.envVar()); len(val) > 0 {
-		result, err := s.imageClient.ImageStreamImports(s.config.Namespace).Create(&imageapi.ImageStreamImport{
-			ObjectMeta: meta.ObjectMeta{
-				Name: "release-import",
-			},
-			Spec: imageapi.ImageStreamImportSpec{
-				Images: []imageapi.ImageImportSpec{
-					{
-						From: coreapi.ObjectReference{
-							Kind: "DockerImage",
-							Name: val,
-						},
-					},
-				},
-			},
-		})
-		if err != nil {
-			if errors.IsForbidden(err) {
-				// the ci-operator expects to have POST /imagestreamimports in the namespace of the tag spec
-				log.Printf("warning: Unable to lock %s to an image digest pull spec, you don't have permission to access the necessary API.", s.envVar())
-				return api.InputDefinition{val}, nil
-			}
-			return nil, err
-		}
-		image := result.Status.Images[0]
-		if image.Image == nil {
-			log.Printf("warning: Unable to lock %s to an image digest pull spec due to an import error (%s): %s.", s.envVar(), image.Status.Reason, image.Status.Message)
-			return api.InputDefinition{val}, nil
-		}
-		log.Printf("Resolved release:%s %s", s.tag(), image.Image.DockerImageReference)
-		s.releaseSpec = image.Image.DockerImageReference
-		return api.InputDefinition{image.Image.Name}, nil
+		return api.InputDefinition{val}, nil
 	}
 	return nil, nil
 }
@@ -329,7 +299,7 @@ func (s *assembleReleaseStep) importFromReleaseImage(ctx context.Context, dry bo
 				return false, nil
 			}
 			if errors.IsForbidden(err) {
-				// the ci-operator expects to have POST /imagestreamimports in the namespace of the tag spec
+				// the ci-operator expects to have POST /imagestreamimports in the namespace of the job
 				log.Printf("warning: Unable to lock %s to an image digest pull spec, you don't have permission to access the necessary API.", s.envVar())
 				return false, nil
 			}
@@ -563,10 +533,10 @@ func hasFailedImportCondition(conditions []imageapi.TagEventCondition, generatio
 }
 
 func (s *assembleReleaseStep) Requires() []api.StepLink {
-	// if our prereq is provided, we only depend on the stable and stable-initial
-	// image streams to be populated
+	// if our prereq is provided, we don't depend on anything as
+	// we will populate the stable streams with our images
 	if s.params.HasInput(s.envVar()) {
-		return []api.StepLink{api.ReleaseImagesLink()}
+		return []api.StepLink{}
 	}
 	if s.name == "latest" {
 		return []api.StepLink{api.ImagesReadyLink()}

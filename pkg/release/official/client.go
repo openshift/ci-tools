dab8407040a3972231e857992fd1404af2586a5c
Steve Kuznetsov <skuznets@redhat.com>
2020-06-03 07:46:59 -0700

    ci-operator: emit better errors when failing to resolve
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- pkg/release/official/client.go
+++ pkg/release/official/client.go
@@ -2,6 +2,7 @@ package official
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -40,6 +41,12 @@ func resolvePullSpec(endpoint string, release api.Release) (string, error) {
 	resp, err := client.Do(req)
 	if err != nil {
 		return "", fmt.Errorf("failed to request latest release: %v", err)
+	}
+	if resp == nil {
+		return "", errors.New("failed to request latest release: got a nil response")
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to request latest release: server responded with %d: %s", resp.StatusCode, resp.Body)
 	}
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {

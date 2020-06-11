11068693e26a1f4e823817b58ae83e3248c95f7a
Steve Kuznetsov <skuznets@redhat.com>
2020-06-11 14:26:53 -0700

    config-brancher: always update all affected versions
    The config-brancher tool works across a number of different versions:
     - the current version devs are working on
     - future versions we've created in advance
     - the version we are bumping the dev version to
    
    Originally, you only had to specify a version in one of these three
    fields to ensure that the config for that version was updated when the
    tool was run. At some point that regressed, so for instance when you
    configured a current version, future versions and a bump version, unless
    you specifically listed the bump version as a future version, the bump
    version would not be updated. This is problematic as when we do a bump,
    the dev configuration is changed to the bump version, and the branch for
    that bump version must also be changed to no longer publish images, or
    we'd have two branches racing to publish to the same image tag. This
    change fixes the bug and adds in a set of integration tests to verify
    it.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- test/integration/ci-operator-config-mirror.sh
+++ test/integration/ci-operator-config-mirror.sh
@@ -3,24 +3,23 @@ source "$(dirname "${BASH_SOURCE}")/../../hack/lib/init.sh"
 trap os::test::junit::reconcile_output EXIT
 
 suite_dir="${OS_ROOT}/test/integration/ci-operator-config-mirror"
-cp -r "${suite_dir}/input" "${BASETMPDIR}"
-cp -r "${suite_dir}/input-to-clean" "${BASETMPDIR}"
-
-actual="${BASETMPDIR}/input"
-actual_to_clean="${BASETMPDIR}/input-to-clean"
-
-expected="${suite_dir}/output"
+workdir="${BASETMPDIR}/ci-operator-config-mirror"
+mkdir -p "${workdir}"
+cp -a "${suite_dir}/"* "${workdir}"
 
 os::test::junit::declare_suite_start "integration/ci-operator-config-mirror"
 # This test validates the ci-operator-config-mirror tool
 
+actual="${workdir}/input"
 os::cmd::expect_success "ci-operator-config-mirror --to-org super-priv --config-path ${actual} --clean=false --whitelist-file ${suite_dir}/whitelist.yaml"
-os::integration::compare "${actual}" "${expected}"
+os::integration::compare "${actual}" "${suite_dir}/output"
 
-os::cmd::expect_success "ci-operator-config-mirror --to-org super-priv --config-path ${actual_to_clean} --clean=true --whitelist-file ${suite_dir}/whitelist.yaml"
-os::integration::compare "${actual_to_clean}" "${expected}"
+actual="${workdir}/input-to-clean"
+os::cmd::expect_success "ci-operator-config-mirror --to-org super-priv --config-path ${actual} --clean=true --whitelist-file ${suite_dir}/whitelist.yaml"
+os::integration::compare "${actual}" "${suite_dir}/output"
 
-os::cmd::expect_success "ci-operator-config-mirror --only-org super --to-org super-priv --config-path ${actual_to_clean} --clean=true --whitelist-file ${suite_dir}/whitelist.yaml"
-os::integration::compare "${actual_to_clean}" "${suite_dir}/output-only-super"
+actual="${workdir}/input-to-clean"
+os::cmd::expect_success "ci-operator-config-mirror --only-org super --to-org super-priv --config-path ${actual} --clean=true --whitelist-file ${suite_dir}/whitelist.yaml"
+os::integration::compare "${actual}" "${suite_dir}/output-only-super"
 
 os::test::junit::declare_suite_end

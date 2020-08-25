62e3e4b4abeb79aa0da64b6f619d89fb4e91f4e1
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2020-08-25 10:22:58 -0400

    CI operator: Tolerate IsAlreadyExists when creating PDBs

--- cmd/ci-operator/main.go
+++ cmd/ci-operator/main.go
@@ -895,7 +895,7 @@ func (o *options) initializeNamespace() error {
 
 	for _, pdbLabelKey := range []string{"openshift.io/build.name", "created-by-ci"} {
 		pdb, mutateFn := pdb(pdbLabelKey, o.namespace)
-		if _, err := crcontrollerutil.CreateOrUpdate(ctx, client, pdb, mutateFn); err != nil {
+		if _, err := crcontrollerutil.CreateOrUpdate(ctx, client, pdb, mutateFn); err != nil && !kerrors.IsAlreadyExists(err) {
 			return fmt.Errorf("failed to create pdb for label key %s: %w", pdbLabelKey, err)
 		}
 		log.Printf("Created PDB for pods with %s label", pdbLabelKey)

de55320beba21bf44db463b26136f3ee3a1643b5
Steve Kuznetsov <skuznets@redhat.com>
2020-09-10 07:05:38 -0700

    slack-bot: introduce barebones serve
    We need a URL to send events and interaction payloads to, so we need a
    server that can handle these and respond as necessary.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- cmd/slack-bot/main.go
+++ cmd/slack-bot/main.go
@@ -1,7 +1,206 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"k8s.io/test-infra/prow/simplifypath"
+
+	"k8s.io/test-infra/pkg/flagutil"
+	"k8s.io/test-infra/prow/config"
+	"k8s.io/test-infra/prow/config/secret"
+	prowflagutil "k8s.io/test-infra/prow/flagutil"
+	"k8s.io/test-infra/prow/interrupts"
+	"k8s.io/test-infra/prow/logrusutil"
+	"k8s.io/test-infra/prow/metrics"
+	"k8s.io/test-infra/prow/pjutil"
+)
+
+type options struct {
+	port int
+
+	logLevel               string
+	gracePeriod            time.Duration
+	instrumentationOptions prowflagutil.InstrumentationOptions
+
+	slackTokenPath         string
+	slackSigningSecretPath string
+}
+
+func (o *options) Validate() error {
+	_, err := logrus.ParseLevel(o.logLevel)
+	if err != nil {
+		return fmt.Errorf("invalid --log-level: %w", err)
+	}
+
+	if o.slackTokenPath == "" {
+		return fmt.Errorf("--slack-token-path is required")
+	}
+
+	if o.slackSigningSecretPath == "" {
+		return fmt.Errorf("--slack-signing-secret-path is required")
+	}
+
+	return nil
+}
+
+func gatherOptions(fs *flag.FlagSet, args ...string) options {
+	var o options
+	fs.IntVar(&o.port, "port", 8888, "Port to listen on.")
+
+	fs.StringVar(&o.logLevel, "log-level", "info", "Level at which to log output.")
+	fs.DurationVar(&o.gracePeriod, "grace-period", 180*time.Second, "On shutdown, try to handle remaining events for the specified duration. ")
+	for _, group := range []flagutil.OptionGroup{&o.instrumentationOptions} {
+		group.AddFlags(fs)
+	}
+
+	fs.StringVar(&o.slackTokenPath, "slack-token-path", "", "Path to the path containing the Slack token to use.")
+	fs.StringVar(&o.slackSigningSecretPath, "slack-signing-secret-path", "", "Path to the path containing the Slack signing secret to use.")
+	if err := fs.Parse(args); err != nil {
+		logrus.WithError(err).Fatal("Could not parse args.")
+	}
+	return o
+}
+
+// l and v keep the tree legible
+func l(fragment string, children ...simplifypath.Node) simplifypath.Node {
+	return simplifypath.L(fragment, children...)
+}
+
+var (
+	promMetrics = metrics.NewMetrics("slack_bot")
+)
 
 func main() {
-	os.Exit(0)
+	logrusutil.ComponentInit()
+
+	o := gatherOptions(flag.NewFlagSet(os.Args[0], flag.ExitOnError), os.Args[1:]...)
+	if err := o.Validate(); err != nil {
+		logrus.WithError(err).Fatal("Invalid options")
+	}
+	level, _ := logrus.ParseLevel(o.logLevel)
+	logrus.SetLevel(level)
+
+	secretAgent := &secret.Agent{}
+	if err := secretAgent.Start([]string{o.slackTokenPath, o.slackSigningSecretPath}); err != nil {
+		logrus.WithError(err).Fatal("Error starting secrets agent.")
+	}
+
+	slackClient := slack.New(string(secretAgent.GetSecret(o.slackTokenPath)))
+
+	metrics.ExposeMetrics("slack-bot", config.PushGateway{}, o.instrumentationOptions.MetricsPort)
+	simplifier := simplifypath.NewSimplifier(l("", // shadow element mimicing the root
+		l("slack",
+			l("interactive-endpoint"),
+			l("events-endpoint"),
+		),
+	))
+	handler := metrics.TraceHandler(simplifier, promMetrics.HTTPRequestDuration, promMetrics.HTTPResponseSize)
+	pjutil.ServePProf(o.instrumentationOptions.PProfPort)
+
+	health := pjutil.NewHealth()
+
+	mux := http.NewServeMux()
+	mux.Handle("/slack/interactive-endpoint", handler(handleInteraction(secretAgent.GetTokenGenerator(o.slackSigningSecretPath), slackClient)))
+	mux.Handle("/slack/events-endpoint", handler(handleEvent(secretAgent.GetTokenGenerator(o.slackSigningSecretPath), slackClient)))
+	server := &http.Server{Addr: ":" + strconv.Itoa(o.port), Handler: mux}
+
+	health.ServeReady()
+
+	interrupts.ListenAndServe(server, o.gracePeriod)
+	interrupts.WaitForGracefulShutdown()
+}
+
+func verifiedBody(logger *logrus.Entry, request *http.Request, signingSecret func() []byte) ([]byte, bool) {
+	verifier, err := slack.NewSecretsVerifier(request.Header, string(signingSecret()))
+	if err != nil {
+		logger.WithError(err).Error("Failed to create a secrets verifier.")
+		return nil, false
+	}
+
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		logger.WithError(err).Error("Failed to read an event payload.")
+		return nil, false
+	}
+
+	// need to use body again when unmarshalling
+	request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	if _, err := verifier.Write(body); err != nil {
+		logger.WithError(err).Error("Failed to hash an event payload.")
+		return nil, false
+	}
+
+	if err = verifier.Ensure(); err != nil {
+		logger.WithError(err).Error("Failed to verify an event payload.")
+		return nil, false
+	}
+
+	return body, true
+}
+
+func handleEvent(signingSecret func() []byte, _ *slack.Client) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		logger := logrus.WithField("api", "events")
+		logger.Debug("Got an event payload.")
+		body, ok := verifiedBody(logger, request, signingSecret)
+		if !ok {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// we are using the newer, more robust signing secret verification so we do
+		// not use the older, deprecated verification token when loading this event
+		event, err := slackevents.ParseEvent(body, slackevents.OptionNoVerifyToken())
+		if err != nil {
+			logger.WithError(err).WithField("body", string(body)).Error("Failed to unmarshal an event payload.")
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		logger.WithField("event", event).Debug("Read an event payload.")
+
+		if event.Type == slackevents.URLVerification {
+			var response *slackevents.ChallengeResponse
+			err := json.Unmarshal(body, &response)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			writer.Header().Set("Content-Type", "text")
+			if _, err := writer.Write([]byte(response.Challenge)); err != nil {
+				logger.WithError(err).Warn("Failed to write response.")
+			}
+		}
+	}
+}
+
+func handleInteraction(signingSecret func() []byte, _ *slack.Client) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		logger := logrus.WithField("api", "interactions")
+		logger.Debug("Got an interaction payload.")
+		if _, ok := verifiedBody(logger, request, signingSecret); !ok {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var interaction slack.InteractionCallback
+		payload := request.FormValue("payload")
+		if err := json.Unmarshal([]byte(payload), &interaction); err != nil {
+			logger.WithError(err).WithField("payload", payload).Error("Failed to unmarshal an interaction payload.")
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		logger.WithField("interaction", interaction).Debug("Read an event payload.")
+	}
 }

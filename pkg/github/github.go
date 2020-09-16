1a64f02ff817ae7e6bdf4d97c2c8288b12230210
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2020-09-16 11:30:36 -0400

    Add authentication support to the github file getter

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -6,6 +6,22 @@ import (
 	"net/http"
 )
 
+type Opts struct {
+	// The username to use for basic auth
+	BasicAuthUser string
+	// The token to use for basic auth
+	BasicAuthPassword string
+}
+
+type Opt func(*Opts)
+
+func WithAuthentication(username, token string) Opt {
+	return func(o *Opts) {
+		o.BasicAuthUser = username
+		o.BasicAuthPassword = token
+	}
+}
+
 // FileGetter is a function that downloads the file from the provided path via raw.githubusercontent.com to avoid getting rate limited.
 // It returns a nil error on 404.
 // TODO: Rethink the 404 behavior?
@@ -13,11 +29,22 @@ type FileGetter func(path string) ([]byte, error)
 
 // FileGetterFactory returns a GithubFileGetter that downloads files from raw.githubusercontent.com for the provided org/repo/branch
 // It avoids getting ratelimited by using raw.githubusercontent.com. Because it is using a plain http client it can be heavily paralellized
-// without killing the machine. It does not support private repositories.
-func FileGetterFactory(org, repo, branch string) FileGetter {
+// without killing the machine. It supports private repositories when configured WithAuthentication.
+func FileGetterFactory(org, repo, branch string, opts ...Opt) FileGetter {
+	o := Opts{}
+	for _, opt := range opts {
+		opt(&o)
+	}
 	return func(path string) ([]byte, error) {
 		url := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/%s/%s", org, repo, branch, path)
-		resp, err := http.DefaultClient.Get(url)
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			return nil, fmt.Errorf("failed to construct request: %w", err)
+		}
+		if o.BasicAuthUser != "" {
+			req.SetBasicAuth(o.BasicAuthUser, o.BasicAuthPassword)
+		}
+		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
 			return nil, fmt.Errorf("failed to GET %s: %w", url, err)
 		}

696d0343e17bda0d39c35f413140dac7ddf7c1b2
Steve Kuznetsov <skuznets@redhat.com>
2020-12-03 15:06:04 -0800

    ipi-deprovision: use the correct artifact location
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- cmd/ipi-deprovision/ipi-deprovision.sh
+++ cmd/ipi-deprovision/ipi-deprovision.sh
@@ -27,7 +27,7 @@ echo "deprovisioning clusters with an expirationDate before ${aws_cluster_age_cu
 for region in $( aws ec2 describe-regions --region us-east-1 --query "Regions[].{Name:RegionName}" --output text ); do
   echo "deprovisioning in AWS region ${region} ..."
   for cluster in $( aws ec2 describe-vpcs --output json --region "${region}" | jq --arg date "${aws_cluster_age_cutoff}" -r -S '.Vpcs[] | select (.Tags[]? | (.Key == "expirationDate" and .Value < $date)) | .Tags[] | select (.Value == "owned") | .Key' ); do
-    workdir="${ARTIFACT_DIR}/deprovision/${cluster:22:14}"
+    workdir="${ARTIFACTS}/deprovision/${cluster:22:14}"
     mkdir -p "${workdir}"
     cat <<EOF >"${workdir}/metadata.json"
 {
@@ -55,7 +55,7 @@ for network in $( gcloud --project=openshift-gce-devel-ci compute networks list 
   if [[ -z "${region:-}" ]]; then
     region=us-east1
   fi
-  workdir="${ARTIFACT_DIR}/deprovision/${infraID}"
+  workdir="${ARTIFACTS}/deprovision/${infraID}"
   mkdir -p "${workdir}"
   cat <<EOF >"${workdir}/metadata.json"
 {
@@ -69,7 +69,7 @@ EOF
   echo "will deprovision GCE cluster ${infraID} in region ${region}"
 done
 
-for workdir in $( find "${ARTIFACT_DIR}/deprovision" -mindepth 1 -type d | shuf ); do
+for workdir in $( find "${ARTIFACTS}/deprovision" -mindepth 1 -type d | shuf ); do
   queue deprovision "${workdir}"
 done
 
@@ -89,7 +89,7 @@ if [[ "${#buckets[@]}" -gt 0 ]]; then
   timeout 30m gsutil -m rm -r "${buckets[@]}"
 fi
 
-FAILED="$(ls "${ARTIFACT_DIR}"/deprovision/*/failure)"
+FAILED="$(ls "${ARTIFACTS}"/deprovision/*/failure)"
 if [[ -n "${FAILED}" ]]; then
   echo "Deprovision failed on the following clusters:"
   echo "${FAILED}"

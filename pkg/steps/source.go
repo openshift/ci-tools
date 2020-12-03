61a7832894f2c66b966742f4be3f6539614d84f7
Steve Kuznetsov <skuznets@redhat.com>
2020-12-03 09:31:55 -0800

    ci-operator: stop explicitly updating submodules
    It is not clear why this change ever landed into our build manifest for
    the source image. The `clonerefs` utility gained this behavior six
    months after the change here landed, and obsoleted it.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- pkg/steps/source.go
+++ pkg/steps/source.go
@@ -94,7 +94,6 @@ func sourceDockerfile(fromTag api.PipelineImageStreamTagReference, workingDir st
 	dockerCommands = append(dockerCommands, fmt.Sprintf("RUN umask 0002 && /clonerefs && find %s/src -type d -not -perm -0775 | xargs -r chmod g+xw", gopath))
 	dockerCommands = append(dockerCommands, fmt.Sprintf("WORKDIR %s/", workingDir))
 	dockerCommands = append(dockerCommands, fmt.Sprintf("ENV GOPATH=%s", gopath))
-	dockerCommands = append(dockerCommands, "RUN git submodule update --init")
 
 	// After the clonerefs command, we don't need the secret anymore.
 	// We don't want to let the key keep existing in the image's layer.

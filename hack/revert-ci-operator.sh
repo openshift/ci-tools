eae06cf47d926c01826c08f92cc0a740dfe45cf1
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2020-12-22 14:50:14 -0500

    Revert ci-operator script: Use correct cluster

--- hack/revert-ci-operator.sh
+++ hack/revert-ci-operator.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 
 OLD_LATEST="$(oc --context api.ci get is ci-operator -n ci -o jsonpath={.status.tags[?\(@.tag==\"latest\"\)].items[1].dockerImageReference}|cut -d '@' -f2)"
 
-echo "execute \`oc --context api.ci tag ci/ci-operator@$OLD_LATEST ci/ci-operator:latest\`"
+echo "execute \`oc --context app.ci tag ci/ci-operator@$OLD_LATEST ci/ci-operator:latest\`"

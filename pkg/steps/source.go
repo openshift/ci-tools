2e9df9c31987e18e294c7a451392e858cb3f2c3f
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2021-01-08 14:24:22 -0500

    Builds: Include the log level env var for documentation
    When CI went down yesterday it took some time to figure out how to set
    the log level for builds, as that is not part of any spec. This changes
    includes the responsible env var together with the default value to make
    finding this setting easier in the future.

--- pkg/steps/source.go
+++ pkg/steps/source.go
@@ -286,7 +286,7 @@ func buildFromSource(jobSpec *api.JobSpec, fromTag, toTag api.PipelineImageStrea
 						From:                    from,
 						ForcePull:               true,
 						NoCache:                 true,
-						Env:                     []corev1.EnvVar{{Name: "foo", Value: "bar"}}, // workaround https://bugzilla.redhat.com/show_bug.cgi?id=1784163#c8
+						Env:                     []corev1.EnvVar{{Name: "BUILD_LOGLEVEL", Value: "0"}}, // this mirrors the default and is done for documentary purposes
 						ImageOptimizationPolicy: &layer,
 					},
 				},

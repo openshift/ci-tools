0c97b03f617bc5535e3625c803fcecb451d3db8a
Hongkai Liu <hongkailiu@users.noreply.github.com>
2021-01-13 13:47:40 -0500

    dptp-cm: add controller image_pusher

--- pkg/api/constant.go
+++ pkg/api/constant.go
@@ -1,6 +1,8 @@
 package api
 
 const (
+	RegistryPullCredentialsSecret = "registry-pull-credentials"
+
 	RegistryPushCredentialsCICentralSecret          = "registry-push-credentials-ci-central"
 	RegistryPushCredentialsCICentralSecretMountPath = "/etc/push-secret"
 
@@ -8,4 +10,7 @@ const (
 	GCSUploadCredentialsSecretMountPath = "/secrets/gcs"
 
 	ReleaseAnnotationSoftDelete = "release.openshift.io/soft-delete"
+
+	// DPTPRequesterLabel is the label on a Kubernates CR whose value indicates the automated tool that requests the CR
+	DPTPRequesterLabel = "dptp.openshift.io/requester"
 )

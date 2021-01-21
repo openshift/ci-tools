49c661c3c1febc07001dbb7711430e613e312b32
Nikolaos Moraitis <nmoraiti@redhat.com>
2021-01-21 15:29:10 +0100

    Revert "ci-operator: release: use a configmap for data transfer"

--- pkg/steps/release/create_release.go
+++ pkg/steps/release/create_release.go
@@ -87,11 +87,6 @@ func setupReleaseImageStream(ctx context.Context, namespace string, client ctrlr
 				Resources: []string{"imagestreams", "imagestreamtags"},
 				Verbs:     []string{"create", "get", "list", "update", "watch"},
 			},
-			{
-				APIGroups: []string{""},
-				Resources: []string{"configmaps"},
-				Verbs:     []string{"create", "get", "list", "update", "watch"},
-			},
 		},
 	}
 

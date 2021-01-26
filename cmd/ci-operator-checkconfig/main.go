73774db752eae45e63e7421b2f9514677725e6de
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2021-01-26 19:05:46 +0000

    checkconfig: resolve configurations
    ```
    $ git rev-parse --short HEAD
    734de06838
    $ time ci-operator-checkconfig --config-dir config/ --registry step-registry/
    
    real    0m1.396s
    user    0m1.984s
    sys     0m0.136s
    $ sed -i '/steps/a\    env: {"INVALID": "invalid"}' config/openshift/ci-tools/openshift-ci-tools-master.yaml
    $ ci-operator-checkconfig --config-dir config/ --registry step-registry/
    ERRO[0000] Failed to execute callback                    error="Failed resolve MultiStageTestConfiguration: e2e: no step declares parameter \"INVALID\"" source-file=/home/bbguimaraes/src/release/ci-operator/config/openshift/ci-tools/openshift-ci-tools-master.yaml
    error validating configuration files: Failed resolve MultiStageTestConfiguration: e2e: no step declares parameter "INVALID"
    ```

--- cmd/ci-operator-checkconfig/main.go
+++ cmd/ci-operator-checkconfig/main.go
@@ -8,25 +8,36 @@ import (
 
 	"github.com/openshift/ci-tools/pkg/api"
 	"github.com/openshift/ci-tools/pkg/config"
+	"github.com/openshift/ci-tools/pkg/load"
+	"github.com/openshift/ci-tools/pkg/registry"
 	"github.com/openshift/ci-tools/pkg/steps/release"
 )
 
 type tagSet map[api.ImageStreamTagReference][]*config.Info
 
 func main() {
-	var configDir string
+	var configDir, registryDir string
 	flag.StringVar(&configDir, "config-dir", "", "The directory containing configuration files.")
+	flag.StringVar(&registryDir, "registry", "", "Path to the step registry directory")
 	flag.Parse()
 
 	if configDir == "" {
 		fmt.Fprintln(os.Stderr, "The --config-dir flag is required but was not provided")
 		os.Exit(1)
 	}
-
+	resolver, err := loadResolver(registryDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load registry: %v\n", err)
+		os.Exit(1)
+	}
 	seen := tagSet{}
 	if err := config.OperateOnCIOperatorConfigDir(configDir, func(configuration *api.ReleaseBuildConfiguration, repoInfo *config.Info) error {
-		// validation is implicit, so we don't need to do anything
-		// but record the images we saw for future validation
+		// basic validation of the configuration is implicit in the iteration
+		if resolver != nil {
+			if _, err := registry.ResolveConfig(resolver, *configuration); err != nil {
+				return err
+			}
+		}
 		for _, tag := range release.PromotedTags(configuration) {
 			seen[tag] = append(seen[tag], repoInfo)
 		}
@@ -42,6 +53,17 @@ func main() {
 		}
 		os.Exit(1)
 	}
+}
+
+func loadResolver(path string) (registry.Resolver, error) {
+	if path == "" {
+		return nil, nil
+	}
+	refs, chains, workflows, _, _, observers, err := load.Registry(path, false)
+	if err != nil {
+		return nil, err
+	}
+	return registry.NewResolver(refs, chains, workflows, observers), nil
 }
 
 func validateTags(seen tagSet) []error {

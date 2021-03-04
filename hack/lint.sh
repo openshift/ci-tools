134b9921d33367cb3bd80cf96e4b78fafb08a103
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2021-03-04 14:12:42 -0500

    Fix lint to wokaround absent file in embedding

--- hack/lint.sh
+++ hack/lint.sh
@@ -9,6 +9,9 @@ if [[ $HOME = '/' ]]; then
   export HOME=/tmp
 fi
 
+# We embedd this so it must exist for compilation to succeed, but it's not checked in
+if [[ -z ${CI:-}]]; then touch cmd/vault-secret-collection-manager/index.js; fi
+
 # The thing has a -skip-{dirs,files} directive which is ignored by half the linters. Why is life so hard.
 targets="$(find . -maxdepth 1 -type d|egrep -v 'git|_output|hack|vendor|^\.$'|sed -E 's/(.*)/\1\/\.\.\./g'|tr '\n' ' ')"
 golangci-lint run --build-tags e2e

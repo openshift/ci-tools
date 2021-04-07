dd23681279e45bf4d99a44a58d68e172f8ab2757
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2021-04-07 09:57:06 -0400

    Fix revert script to correctly point to app.ci

--- hack/revert-ci-operator.sh
+++ hack/revert-ci-operator.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 
-OLD_LATEST="$(oc --context api.ci get is ci-operator -n ci -o jsonpath={.status.tags[?\(@.tag==\"latest\"\)].items[1].dockerImageReference}|cut -d '@' -f2)"
+OLD_LATEST="$(oc --context app.ci get is ci-operator -n ci -o jsonpath={.status.tags[?\(@.tag==\"latest\"\)].items[1].dockerImageReference}|cut -d '@' -f2)"
 
 echo "execute \`oc --context app.ci tag ci/ci-operator@$OLD_LATEST ci/ci-operator:latest\`"

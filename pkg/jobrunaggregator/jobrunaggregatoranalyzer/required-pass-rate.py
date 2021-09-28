08441d6a661dc5172084f6c64506084040900188
David Eads <deads@redhat.com>
2021-09-28 17:23:24 -0400

    perform python abominations to get it to print the fisher exact pass count for aggregation

--- pkg/jobrunaggregator/jobrunaggregatoranalyzer/required-pass-rate.py
+++ pkg/jobrunaggregator/jobrunaggregatoranalyzer/required-pass-rate.py
@@ -9,53 +9,62 @@ import matplotlib.pyplot as plt
 
 # We want to determine if the two samples (the corpus from accepted releases and
 # the smaller set of results from the testing of a PR) come from binary distributions
-# that are dissimilar (have different underlying proportions). We use the Fisher exact
+# that are dissimilar (have different underlying percentages). We use the Fisher exact
 # test, as our sample sizes are small: https://www.itl.nist.gov/div898/handbook/prc/section3/prc33.htm
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-test_size = 10
 significance = 0.05
+corpus_size = 250
 
-for corpus_size in [250]:
-	corpus_proportions = numpy.arange(0,1.01,.01)
-	test_proportions = numpy.arange(0,1.1,.1)
+for test_size in [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]:
+	corpus_percentages = numpy.arange(0, 101, 1)
+	test_positive_counts = numpy.arange(0, test_size+1, 1)
+	min_positive_counts = []
+	
+	for corpus_percentage in corpus_percentages:
+		found = False
+		for test_positive_count in test_positive_counts:
+			corpus_positive_count = round(corpus_size * corpus_percentage/100)
+			corpus_negative_count = corpus_size - corpus_positive_count
+			test_negative_count = test_size - test_positive_count
+			# leave this debugging line for david because he doesn't speak python
+ 			# print("checking %d %d %d %d\n" % (corpus_positive_count, test_positive_count, corpus_negative_count, test_negative_count))
 
-	@numpy.vectorize
-	def P(corpus_proportion, test_proportion, alternative):
-		corpus_positive_count = round(corpus_size * corpus_proportion)
-		corpus_negative_count = corpus_size - corpus_positive_count
-		test_positive_count = round(test_size * test_proportion)
-		test_negative_count = test_size - test_positive_count
-		_, significance = fisher_exact([
-			[corpus_positive_count, test_positive_count],
-			[corpus_negative_count, test_negative_count],
-			], alternative=alternative)
-		return significance
+			_, p = fisher_exact([
+				[corpus_positive_count, test_positive_count],
+				[corpus_negative_count, test_negative_count],
+				], alternative="greater")
 
-	x, y = numpy.meshgrid(corpus_proportions, test_proportions)
-
-	greater = P(x,y,'greater')
-	max_min = []
-	for c in range(greater.shape[1]):
-		previous = numpy.NAN
-		for idx, p in enumerate(greater[:,c]):
 			if p <= significance:
-				previous = idx / test_size
+				continue
 			else:
-				max_min.append(previous)
+				# if we get here then we went one past significance
+				if test_positive_count == 0:
+					min_positive_counts.append(test_positive_count)
+				else:
+					min_positive_counts.append(test_positive_count-1)
+				# leave this debugging line for david because he doesn't speak python
+				# print("percent %d, min_positive_count %d" % (corpus_percentage, test_positive_count))
+				found = True
 				break
+		if not found:
+			if corpus_percentage == 100:
+				min_positive_counts.append(min_positive_counts[98])
+			else:
+				min_positive_counts.append(0)
 
-	# mesh = ax.pcolormesh(x, y, z, vmin=0, vmax=1, cmap=cm.viridis, shading='nearest')
-	color = next(ax._get_lines.prop_cycler)['color']
-	ax.plot(corpus_proportions, max_min, color=color)
-	ax.text(1.005, max_min[-1], 'n={}'.format(corpus_size), color=color)
+	for i, required_pass in enumerate(min_positive_counts):
+		if i % 10 == 0:
+			print()
+		print("%d, " % (required_pass), end='')
+		if i % 10 == 9:
+			print(" // %d-%d" % (i-9, i), end='')
+	print()
 
-ax.set_aspect('equal', adjustable='box')
-ax.set_ylim(0,1)
-plt.ylabel('Observed Test Pass Proportion')
-ax.set_xlim(0,1)
-plt.xlabel('Observed Corpus Pass Proportion')
-plt.title('Test Passes Required For Statistical Significance')
-plt.show()
+# _, p = fisher_exact([
+# 	[125, 1],
+# 	[125, 9],
+# 	], alternative="greater")
+# print(p)

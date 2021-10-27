ca86d844fbd59cbc9db391140d337d8473aa8ae3
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2021-10-27 16:41:39 +0000

    pkg/webreg: extract base code to a module

---
+++ pkg/html/html.go
@@ -0,0 +1,52 @@
+package html
+
+import (
+	_ "embed"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+//go:embed base.css
+var baseCSS string
+
+var pageStart = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"><title>%s</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<style>` + baseCSS + `</style>
+</head>
+<body>
+`
+
+const pageEnd = `</body>
+</html>
+`
+
+func WritePage(w http.ResponseWriter, title, bodyStart, end string, body *template.Template, data interface{}) error {
+	e := func(err error) error {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s: %v", http.StatusText(http.StatusInternalServerError), err)
+		return err
+	}
+	if _, err := fmt.Fprintf(w, pageStart, title); err != nil {
+		return e(err)
+	}
+	if _, err := fmt.Fprintln(w, bodyStart); err != nil {
+		return e(err)
+	}
+	if err := body.Execute(w, data); err != nil {
+		return e(err)
+	}
+	if _, err := fmt.Fprintln(w, end); err != nil {
+		return e(err)
+	}
+	if _, err := fmt.Fprintln(w, pageEnd); err != nil {
+		return e(err)
+	}
+	return nil
+}
--- pkg/webreg/webreg.go
+++ pkg/webreg/webreg.go
@@ -2,7 +2,6 @@ package webreg
 
 import (
 	"bytes"
-	_ "embed"
 	"errors"
 	"fmt"
 	"html/template"
@@ -12,7 +11,7 @@ import (
 	"time"
 
 	"github.com/alecthomas/chroma"
-	"github.com/alecthomas/chroma/formatters/html"
+	htmlformatter "github.com/alecthomas/chroma/formatters/html"
 	"github.com/alecthomas/chroma/lexers"
 	"github.com/alecthomas/chroma/styles"
 	"github.com/sirupsen/logrus"
@@ -21,6 +20,7 @@ import (
 	"k8s.io/test-infra/prow/repoowners"
 
 	"github.com/openshift/ci-tools/pkg/api"
+	"github.com/openshift/ci-tools/pkg/html"
 	"github.com/openshift/ci-tools/pkg/load"
 	"github.com/openshift/ci-tools/pkg/load/agents"
 	"github.com/openshift/ci-tools/pkg/registry"
@@ -47,20 +47,7 @@ const (
 	TestQuery = "test"
 )
 
-//go:embed base.css
-var baseCSS string
-var htmlPageStart = `
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8"><title>%s</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<style>` + baseCSS + `</style>
-</head>
-<body>
+const bodyStart = `
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="/">Openshift CI Step Registry</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -91,11 +78,9 @@ var htmlPageStart = `
 <div class="container">
 `
 
-const htmlPageEnd = `
+const bodyEnd = `
 <p class="small">Source code for this page located on <a href="https://github.com/openshift/ci-tools">GitHub</a></p>
-</div>
-</body>
-</html>`
+</div>`
 
 const errPage = `
 {{ . }}
@@ -780,13 +765,7 @@ func writeErrorPage(w http.ResponseWriter, pageErr error, status int) {
 }
 
 func writePage(w http.ResponseWriter, title string, body *template.Template, data interface{}) {
-	fmt.Fprintf(w, htmlPageStart, title)
-	if err := body.Execute(w, data); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "%s: %v", http.StatusText(http.StatusInternalServerError), err)
-		return
-	}
-	fmt.Fprintln(w, htmlPageEnd)
+	_ = html.WritePage(w, title, bodyStart, bodyEnd, body, data)
 }
 
 func setDocs(t *template.Template, docs map[string]string) *template.Template {
@@ -1225,7 +1204,7 @@ func syntax(source string, lexer chroma.Lexer) (string, error) {
 	var output bytes.Buffer
 	style := styles.Get("dracula")
 	// highlighted lines based on linking currently require WithClasses to be used
-	formatter := html.New(html.Standalone(false), html.LinkableLineNumbers(true, "line"), html.WithLineNumbers(true), html.LineNumbersInTable(true), html.WithClasses(true))
+	formatter := htmlformatter.New(htmlformatter.Standalone(false), htmlformatter.LinkableLineNumbers(true, "line"), htmlformatter.WithLineNumbers(true), htmlformatter.LineNumbersInTable(true), htmlformatter.WithClasses(true))
 	iterator, err := lexer.Tokenise(nil, source)
 	if err != nil {
 		return "", fmt.Errorf("failed to tokenise source: %w", err)

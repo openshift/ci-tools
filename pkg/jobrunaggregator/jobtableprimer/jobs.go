b3d00a123ade57fa5a7d70abd500795f6be9f690
David Eads <deads@redhat.com>
2022-01-05 13:49:32 -0500

    read generated content to find jobs to aggregate

--- pkg/jobrunaggregator/jobtableprimer/jobs.go
+++ pkg/jobrunaggregator/jobtableprimer/jobs.go
@@ -1,6 +1,11 @@
 package jobtableprimer
 
-import "github.com/openshift/ci-tools/pkg/jobrunaggregator/jobrunaggregatorapi"
+import (
+	_ "embed"
+	"strings"
+
+	"github.com/openshift/ci-tools/pkg/jobrunaggregator/jobrunaggregatorapi"
+)
 
 const (
 	gcp   = "gcp"
@@ -50,63 +55,37 @@ var (
 		newJob("periodic-ci-openshift-release-master-ci-4.9-e2e-gcp").ToJob(),
 		newJob("periodic-ci-openshift-release-master-nightly-4.9-e2e-aws").ToJob(),
 		newJob("periodic-ci-openshift-release-master-nightly-4.9-e2e-aws-serial").ToJob(),
-
-		// 4.10 CI
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-gcp-upgrade").WithE2EParallel().ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-aws-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-aws-ovn-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-aws-ovn-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-aws-serial").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-gcp").ToJob(),
-		// 4.10 nightly
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-azure-upgrade-single-node").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-ipi-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-upgrade-from-stable-4.9-e2e-metal-ipi-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-azure-ovn-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-gcp-ovn-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-upgrade-from-stable-4.9-e2e-azure-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-aws-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-upgrade-from-stable-4.9-e2e-aws-upgrade").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-aws").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-aws-serial").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-ipi").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-ipi-ovn-ipv6").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-ipi-serial-ipv4").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-console-aws").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-aws-fips").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-aws-ovn").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-aws-single-node").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-aws-single-node-serial").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-aws-techpreview").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-aws-techpreview-serial").ToJob(),
-		newJob("release-openshift-ocp-installer-e2e-aws-upi-4.10").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-azure").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-azure-ovn").ToJob(),
-		newJob("release-openshift-ocp-installer-e2e-azure-serial-4.10").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-azure-techpreview").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-azure-techpreview-serial").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-gcp").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-gcp-ovn").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-gcp-rt").ToJob(),
-		newJob("release-openshift-ocp-installer-e2e-gcp-serial-4.10").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-gcp-techpreview").ToJob(),
-		newJob("periodic-ci-openshift-release-master-ci-4.10-e2e-gcp-techpreview-serial").ToJob(),
-		newJob("release-openshift-ocp-installer-e2e-metal-4.10").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-assisted").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-assisted-ipv6").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-ipi-ovn-dualstack").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-ipi-virtualmedia").WithoutDisruption().ToJob(),
-		newJob("release-openshift-ocp-installer-e2e-metal-serial-4.10").ToJob(),
-		newJob("release-openshift-ocp-osd-aws-nightly-4.10").WithoutDisruption().ToJob(),
-		newJob("release-openshift-ocp-osd-gcp-nightly-4.10").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-ovirt").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-aws-proxy").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-metal-single-node-live-iso").WithoutDisruption().ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-vsphere").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-vsphere-serial").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-vsphere-techpreview").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-vsphere-techpreview-serial").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-vsphere-upi").ToJob(),
-		newJob("periodic-ci-openshift-release-master-nightly-4.10-e2e-vsphere-upi-serial").ToJob(),
 	}
 )
+
+//go:embed generated_job_names.txt
+var jobNames string
+
+func init() {
+	for _, jobName := range strings.Split(jobNames, "\n") {
+		// skip comments
+		if strings.HasPrefix(jobName, "//") {
+			continue
+		}
+
+		// skip empty lines
+		jobName = strings.TrimSpace(jobName)
+		if len(jobName) == 0 {
+			continue
+		}
+
+		// skip duplicates.  This happens when periodics redefine release config ones.
+		found := false
+		for _, existing := range jobsToAnalyze {
+			if existing.JobName == jobName {
+				found = true
+				break
+			}
+		}
+		if found {
+			continue
+		}
+
+		jobsToAnalyze = append(jobsToAnalyze, newJob(jobName).ToJob())
+	}
+}

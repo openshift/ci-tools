ef35af92b2a3435f79d860cd904ae440e40c2ca3
Vojtěch Kronika <vkronika@redhat.com>
2022-03-21 08:59:55 +0100

    Update TestGatherOptions
    Changed comparison from stringSlicesEqual to cmp.Diff.
    Removed method stringsSlicesEqual.

--- cmd/better-retester/main_test.go
+++ cmd/better-retester/main_test.go
@@ -3,26 +3,17 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func stringSlicesEqual(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
-	}
-	return true
-}
 func TestGatherOptions(t *testing.T) {
 	expected := []string{"org/repo", "other-org/other-repo"}
 	os.Args = []string{"cmd", "--enable-on-repo=org/repo", "--cache-record-age=100h", "--enable-on-repo=other-org/other-repo"}
 
 	actual := gatherOptions()
 
-	if !stringSlicesEqual(actual.enableOnRepos.Strings(), expected) {
+	if diff := cmp.Diff(expected, actual.enableOnRepos.Strings()); diff != "" {
 		t.Errorf("Test failed, expected: '%+v', got:  '%+v'", expected, actual.enableOnRepos)
 	}
 }

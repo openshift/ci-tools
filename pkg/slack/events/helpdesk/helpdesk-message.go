dc68949bc675ce048d2a4438eaa0a09dd5126484
Patricia Salajova <psalajov@redhat.com>
2022-04-07 10:40:31 +0200

    Add automatic ephemeral reply for helpdesk

--- pkg/slack/events/helpdesk/helpdesk-message.go
+++ pkg/slack/events/helpdesk/helpdesk-message.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/sirupsen/logrus"
+	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 
 	"github.com/openshift/ci-tools/pkg/slack/events"
@@ -14,9 +15,13 @@ const (
 	dptpHelpdeskId = "<@STSR51Q76>"
 )
 
+type ephemeralMessagePoster interface {
+	PostEphemeral(channelID, userID string, options ...slack.MsgOption) (string, error)
+}
+
 // Handler returns a handler that knows how to respond to new messages
 // in forum-testplatform channel that mention @dptp-helpdesk.
-func Handler() events.PartialHandler {
+func Handler(client ephemeralMessagePoster) events.PartialHandler {
 	return events.PartialHandlerFunc("helpdesk",
 		func(callback *slackevents.EventsAPIEvent, logger *logrus.Entry) (handled bool, err error) {
 
@@ -39,6 +44,40 @@ func Handler() events.PartialHandler {
 			}
 			logger.Info("Handling response in forum-testplatform channel...")
 
+			timestamp := event.TimeStamp
+			if event.ThreadTimeStamp != "" {
+				timestamp = event.ThreadTimeStamp
+			}
+
+			responseTimestamp, err := client.PostEphemeral(event.Channel, event.User, slack.MsgOptionBlocks(getResponse()...), slack.MsgOptionTS(timestamp))
+			if err != nil {
+				logger.WithError(err).Warn("Failed to post ephemeral response")
+			} else {
+				logger.Infof("Posted ephemeral message in channel %s to user %s at %s", event.Channel, event.User, responseTimestamp)
+			}
+
 			return true, err
 		})
+}
+
+func getResponse() []slack.Block {
+	sections := []string{
+		":wave: You have reached the Test Platform Help Desk. An assigned engineer will respond in several hours during their working hours.",
+		"Please see if our documentation can be of use: https://docs.ci.openshift.org/docs/",
+		"If this is an urgent CI outage, please ping `(@)dptp-triage`",
+	}
+
+	var blocks []slack.Block
+
+	for _, section := range sections {
+		blocks = append(blocks, &slack.SectionBlock{
+			Type: slack.MBTSection,
+			Text: &slack.TextBlockObject{
+				Type: slack.MarkdownType,
+				Text: section,
+			},
+		})
+	}
+
+	return blocks
 }

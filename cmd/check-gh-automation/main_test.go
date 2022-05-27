d166f73b4434d5b2c5c4bd3477677a62f15bd47c
Stephen Goeddel <sgoeddel@redhat.com>
2022-05-27 05:26:58 -0400

    Create `check-gh-automation` tool (#2834)
    * create check-gh-automation tool
    
    * fix imports
    
    * use git rather than loading prow configs to determine changes
    
    * add unit test for error scenario

---
+++ cmd/check-gh-automation/main_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/sirupsen/logrus"
+
+	"github.com/openshift/ci-tools/pkg/testhelper"
+)
+
+type fakeCollaboratorClient struct {
+	repos map[string][]string
+}
+
+func (c fakeCollaboratorClient) IsCollaborator(org, repo, user string) (bool, error) {
+	orgRepo := fmt.Sprintf("%s/%s", org, repo)
+	collaborators := c.repos[orgRepo]
+	for _, collaborator := range collaborators {
+		if collaborator == user {
+			return true, nil
+		}
+	}
+	if repo == "fake" {
+		return false, errors.New("intentional error")
+	}
+
+	return false, nil
+}
+
+func TestCheckRepos(t *testing.T) {
+	client := fakeCollaboratorClient{repos: map[string][]string{
+		"org-1/repo-a": {"a-bot", "b-bot", "c-bot"},
+		"org-2/repo-z": {"c-bot", "some-user"},
+	}}
+
+	testCases := []struct {
+		name        string
+		repos       []string
+		bots        []string
+		expected    []string
+		expectedErr error
+	}{
+		{
+			name:  "repo has bots as collaborators",
+			repos: []string{"org-1/repo-a"},
+			bots:  []string{"a-bot", "b-bot"},
+		},
+		{
+			name:     "repo doesn't have bots as collaborators",
+			repos:    []string{"org-2/repo-z"},
+			bots:     []string{"a-bot", "b-bot"},
+			expected: []string{"org-2/repo-z"},
+		},
+		{
+			name:     "multiple repos, some passing",
+			repos:    []string{"org-1/repo-a", "org-2/repo-z"},
+			bots:     []string{"a-bot", "b-bot"},
+			expected: []string{"org-2/repo-z"},
+		},
+		{
+			name:        "collaborator check returns error",
+			repos:       []string{"org-1/fake"},
+			bots:        []string{"a-bot"},
+			expectedErr: errors.New("unable to determine if: a-bot is a collaborator on org-1/fake: intentional error"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			failing, err := checkRepos(tc.repos, tc.bots, client, logrus.NewEntry(logrus.New()))
+			if diff := cmp.Diff(tc.expectedErr, err, testhelper.EquateErrorMessage); diff != "" {
+				t.Fatalf("error doesn't match expected, diff: %s", diff)
+			}
+			if diff := cmp.Diff(tc.expected, failing); diff != "" {
+				t.Fatalf("returned failing repos did not match expected, diff: %s", diff)
+			}
+		})
+	}
+}

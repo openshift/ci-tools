038197dbb2654031a387e10bbaceff9bec9cccf7
Hongkai Liu <hongkailiu@users.noreply.github.com>
2022-08-09 23:49:38 -0400

    waitForBuild: use watch instead of get

--- pkg/steps/source_test.go
+++ pkg/steps/source_test.go
@@ -1,16 +1,26 @@
 package steps
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
 
 	coreapi "k8s.io/api/core/v1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
 	prowapi "k8s.io/test-infra/prow/apis/prowjobs/v1"
 	"k8s.io/test-infra/prow/pod-utils/downwardapi"
+	fakectrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	buildapi "github.com/openshift/api/build/v1"
 
 	"github.com/openshift/ci-tools/pkg/api"
+	"github.com/openshift/ci-tools/pkg/steps/loggingclient"
 	"github.com/openshift/ci-tools/pkg/testhelper"
 )
 
@@ -367,4 +377,81 @@ func TestBuildFromSource(t *testing.T) {
 			testhelper.CompareWithFixture(t, actual)
 		})
 	}
+}
+
+func init() {
+	if err := buildapi.AddToScheme(scheme.Scheme); err != nil {
+		panic(fmt.Sprintf("failed to add hivev1 to scheme: %v", err))
+	}
+}
+
+func TestWaitForBuild(t *testing.T) {
+	var testCases = []struct {
+		name        string
+		buildClient BuildClient
+		expected    error
+	}{
+		{
+			name:        "timeout",
+			buildClient: NewBuildClient(loggingclient.New(fakectrlruntimeclient.NewClientBuilder().WithRuntimeObjects().Build()), nil),
+			expected:    fmt.Errorf("timed out waiting for the condition"),
+		},
+		{
+			name: "build succeeded",
+			buildClient: NewBuildClient(loggingclient.New(fakectrlruntimeclient.NewClientBuilder().WithRuntimeObjects(
+				&buildapi.Build{
+					ObjectMeta: meta.ObjectMeta{
+						Name:      "some-build",
+						Namespace: "some-ns",
+					},
+					Status: buildapi.BuildStatus{
+						Phase: buildapi.BuildPhaseComplete,
+					},
+				}).Build()), nil),
+		},
+		{
+			name: "build failed",
+			buildClient: NewFakeBuildClient(loggingclient.New(fakectrlruntimeclient.NewClientBuilder().WithRuntimeObjects(
+				&buildapi.Build{
+					ObjectMeta: meta.ObjectMeta{
+						Name:      "some-build",
+						Namespace: "some-ns",
+					},
+					Status: buildapi.BuildStatus{
+						Phase:      buildapi.BuildPhaseCancelled,
+						Reason:     "reason",
+						Message:    "msg",
+						LogSnippet: "snippet",
+					},
+				}).Build()), "abc"),
+			expected: fmt.Errorf("%s\n\n%s", "the build some-build failed after 3s with reason reason: msg", "snippet"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			actual := waitForBuild(context.TODO(), testCase.buildClient, "some-ns", "some-build", 90*time.Millisecond, func(build *buildapi.Build) time.Duration {
+				return 3 * time.Second
+			})
+			if diff := cmp.Diff(testCase.expected, actual, testhelper.EquateErrorMessage); diff != "" {
+				t.Errorf("%s: mismatch (-expected +actual), diff: %s", testCase.name, diff)
+			}
+		})
+	}
+}
+
+type fakeBuildClient struct {
+	loggingclient.LoggingClient
+	logContent string
+}
+
+func NewFakeBuildClient(client loggingclient.LoggingClient, logContent string) BuildClient {
+	return &fakeBuildClient{
+		LoggingClient: client,
+		logContent:    logContent,
+	}
+}
+
+func (c *fakeBuildClient) Logs(namespace, name string, options *buildapi.BuildLogOptions) (io.ReadCloser, error) {
+	return io.NopCloser(strings.NewReader(c.logContent)), nil
 }

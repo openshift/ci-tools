4212d37f63d8cd7ce67e03c952abe6ee243d71ff
Stephen Goeddel <sgoeddel@redhat.com>
2022-08-19 09:43:59 -0400

    remove defunt preserveUnknownFields option

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -2,14 +2,14 @@
 
 set -euxo pipefail
 
-go run ./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen crd:preserveUnknownFields=false object \
+go run ./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen object \
     paths=./pkg/api/testimagestreamtagimport/v1 \
     output:dir=./pkg/api/testimagestreamtagimport/v1
 
-go run ./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen crd:preserveUnknownFields=false object \
+go run ./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen object \
     paths=./pkg/api/pullrequestpayloadqualification/v1 \
     output:dir=./pkg/api/pullrequestpayloadqualification/v1
 
-go run ./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen crd:preserveUnknownFields=false object \
+go run ./vendor/sigs.k8s.io/controller-tools/cmd/controller-gen object \
     paths=./pkg/api/ \
     output:dir=./pkg/api/

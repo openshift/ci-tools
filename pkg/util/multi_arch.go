331e29d7f1c9438f4439bb6c43ae752900fdebc3
Nikolaos Moraitis <nmoraiti@redhat.com>
2022-08-23 11:38:32 +0200

    improve logging when resolving namespace for the os architecture
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

--- pkg/util/multi_arch.go
+++ pkg/util/multi_arch.go
@@ -9,11 +9,14 @@ import (
 
 // ResolveMultiArchNamespaceFor returns the namespace name based on the os architecture
 func ResolveMultiArchNamespaceFor(namespace string) string {
+	var ret string
 	arch := runtime.GOARCH
+	logrus.Infof("Running on %s architecture", arch)
 	if arch == "amd64" {
-		return namespace
+		ret = namespace
+	} else {
+		ret = fmt.Sprintf("%s-%s", namespace, arch)
 	}
-	ret := fmt.Sprintf("%s-%s", namespace, arch)
-	logrus.Infof("Resolved multi-arch namespace for %s to %s", namespace, ret)
+	logrus.Infof("Resolved multi-arch namespace for %s to %s for %s architecture", namespace, ret, arch)
 	return ret
 }

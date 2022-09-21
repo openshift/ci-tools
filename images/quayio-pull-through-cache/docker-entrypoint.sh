8ad009618ab273054a73b47df1273c568ec2b2fd
Jakub Guzik <jguzik@redhat.com>
2022-09-21 21:29:31 +0200

    Correct image misconfiguration: quay cache
    Signed-off-by: Jakub Guzik <jguzik@redhat.com>

--- images/quayio-pull-through-cache/docker-entrypoint.sh
+++ images/quayio-pull-through-cache/docker-entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 set -e
-
 secret_path=/etc/pull-secret/.dockerconfigjson
-ls -l -S $secret_path 2>/dev/null || echo "Missing $secret_path" && exit 1
 export REGISTRY_PROXY_USERNAME=$(cat $secret_path | jq '.auths["quay.io"].auth' -r | base64 -d | cut -d ':'  -f 1)
 export REGISTRY_PROXY_PASSWORD=$(cat $secret_path | jq '.auths["quay.io"].auth' -r | base64 -d | cut -d ':'  -f 2)
-htpasswd -Bbn ${REGISTRY_PROXY_USERNAME} ${REGISTRY_PROXY_PASSWORD} > /etc/htpasswd
-
+htpasswd -Bbn ${REGISTRY_PROXY_USERNAME} ${REGISTRY_PROXY_PASSWORD} > /tmp/htpasswd
 config="/etc/docker/registry/config.yml"
-ls -l -S $config 2>/dev/null || echo "Missing $config" && exit 1
 registry serve $config &
 pid=$!
-
 while true; do
     quay_io_username=$(cat $secret_path | jq '.auths["quay.io"].auth' -r | base64 -d | cut -d ':'  -f 1)
     quay_io_password=$(cat $secret_path | jq '.auths["quay.io"].auth' -r | base64 -d | cut -d ':'  -f 2)

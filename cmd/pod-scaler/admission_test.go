c2cf1d1f258b09148d5265f181041ffb6884927c
Stephen Goeddel <sgoeddel@redhat.com>
2023-02-10 14:10:25 -0500

    add priority scheduling class when CPU request is above a certain amount

--- cmd/pod-scaler/admission_test.go
+++ cmd/pod-scaler/admission_test.go
@@ -1044,3 +1044,77 @@ func TestDetermineWorkloadType(t *testing.T) {
 		})
 	}
 }
+
+func TestAddPriorityClass(t *testing.T) {
+	testCases := []struct {
+		name     string
+		pod      *corev1.Pod
+		expected *corev1.Pod
+	}{
+		{
+			name: "cpu under configured amount for priority scheduling",
+			pod: &corev1.Pod{Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("1")}}},
+				},
+			}},
+			expected: &corev1.Pod{Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("1")}}},
+				},
+			}},
+		},
+		{
+			name: "cpu of exactly configured amount for priority scheduling",
+			pod: &corev1.Pod{Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("8")}}},
+				},
+			}},
+			expected: &corev1.Pod{Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("8")}}},
+				},
+				PriorityClassName: priorityClassName,
+			}},
+		},
+		{
+			name: "cpu above configured amount for priority scheduling",
+			pod: &corev1.Pod{Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("9")}}},
+				},
+			}},
+			expected: &corev1.Pod{Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("9")}}},
+				},
+				PriorityClassName: priorityClassName,
+			}},
+		},
+		{
+			name: "cpu of initContainer above configured amount for priority scheduling",
+			pod: &corev1.Pod{Spec: corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("9")}}},
+				},
+			}},
+			expected: &corev1.Pod{Spec: corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("9")}}},
+				},
+				PriorityClassName: priorityClassName,
+			}},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			m := podMutator{cpuPriorityScheduling: 8}
+			m.addPriorityClass(tc.pod)
+			if diff := cmp.Diff(tc.pod, tc.expected); diff != "" {
+				t.Fatalf("expected pod doesn't match actual, diff: %s", diff)
+			}
+		})
+	}
+}
--- cmd/pod-scaler/main.go
+++ cmd/pod-scaler/main.go
@@ -60,11 +60,12 @@ type consumerOptions struct {
 	port   int
 	uiPort int
 
-	dataDir              string
-	certDir              string
-	mutateResourceLimits bool
-	cpuCap               int64
-	memoryCap            string
+	dataDir               string
+	certDir               string
+	mutateResourceLimits  bool
+	cpuCap                int64
+	memoryCap             string
+	cpuPriorityScheduling int64
 }
 
 func bindOptions(fs *flag.FlagSet) *options {
@@ -86,6 +87,7 @@ func bindOptions(fs *flag.FlagSet) *options {
 	fs.StringVar(&o.gcsCredentialsFile, "gcs-credentials-file", "", "File where GCS credentials are stored.")
 	fs.Int64Var(&o.cpuCap, "cpu-cap", 10, "The maximum CPU request value, ex: 10")
 	fs.StringVar(&o.memoryCap, "memory-cap", "20Gi", "The maximum memory request value, ex: '20Gi'")
+	fs.Int64Var(&o.cpuPriorityScheduling, "cpu-priority-scheduling", 8, "Pods with CPU requests at, or above, this value will be admitted with priority scheduling")
 	o.resultsOptions.Bind(fs)
 	return &o
 }
@@ -257,7 +259,7 @@ func mainAdmission(opts *options, cache cache) {
 		logrus.WithError(err).Fatal("Failed to create pod-scaler reporter.")
 	}
 
-	go admit(opts.port, opts.instrumentationOptions.HealthPort, opts.certDir, client, loaders(cache), opts.mutateResourceLimits, opts.cpuCap, opts.memoryCap, reporter)
+	go admit(opts.port, opts.instrumentationOptions.HealthPort, opts.certDir, client, loaders(cache), opts.mutateResourceLimits, opts.cpuCap, opts.memoryCap, opts.cpuPriorityScheduling, reporter)
 }
 
 func loaders(cache cache) map[string][]*cacheReloader {

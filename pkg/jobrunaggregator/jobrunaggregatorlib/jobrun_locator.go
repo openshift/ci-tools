f606764a0717fa1b4ff9959ebf65ec4c93fb9dd7
Forrest Babcock <36795216+neisw@users.noreply.github.com>
2023-06-14 17:34:52 -0400

    Revert "reduce job run query size"

--- pkg/jobrunaggregator/jobrunaggregatorlib/jobrun_locator.go
+++ pkg/jobrunaggregator/jobrunaggregatorlib/jobrun_locator.go
@@ -24,7 +24,7 @@ var (
 )
 
 type JobRunLocator interface {
-	FindRelatedJobRuns(ctx context.Context) ([]jobrunaggregatorapi.JobRunInfo, error)
+	FindRelatedJobs(ctx context.Context) ([]jobrunaggregatorapi.JobRunInfo, error)
 }
 
 type prowJobMatcherFunc func(prowJob *prowjobv1.ProwJob) bool
@@ -66,9 +66,9 @@ func NewPayloadAnalysisJobLocator(
 	}
 }
 
-// FindRelatedJobRuns returns a slice of JobRunInfo which has info contained in GCS buckets
+// FindRelatedJobs returns a slice of JobRunInfo which has info contained in GCS buckets
 // used to determine pass/fail.
-func (a *analysisJobAggregator) FindRelatedJobRuns(ctx context.Context) ([]jobrunaggregatorapi.JobRunInfo, error) {
+func (a *analysisJobAggregator) FindRelatedJobs(ctx context.Context) ([]jobrunaggregatorapi.JobRunInfo, error) {
 	startOfJobRunWindow := a.startTime.Add(-1 * JobSearchWindowStartOffset)
 	endOfJobRunWindow := a.startTime.Add(JobSearchWindowEndOffset)
 	startingJobRun, err := a.ciDataClient.GetJobRunForJobNameBeforeTime(ctx, a.jobName, startOfJobRunWindow)
@@ -94,15 +94,15 @@ func (a *analysisJobAggregator) FindRelatedJobRuns(ctx context.Context) ([]jobru
 	if err := query.SetAttrSelection([]string{"Name", "Created"}); err != nil {
 		return nil, err
 	}
-	if startingJobRun == "" {
+	if startingJobRun == nil {
 		// For debugging, you can set this to a jobID that is not that far away from
 		// jobs related to what you are trying to aggregate.
 		query.StartOffset = fmt.Sprintf("%s/%s", a.gcsPrefix, "0")
 	} else {
-		query.StartOffset = fmt.Sprintf("%s/%s", a.gcsPrefix, startingJobRun)
+		query.StartOffset = fmt.Sprintf("%s/%s", a.gcsPrefix, startingJobRun.Name)
 	}
-	if endingJobRun != "" {
-		query.EndOffset = fmt.Sprintf("%s/%s", a.gcsPrefix, endingJobRun)
+	if endingJobRun != nil {
+		query.EndOffset = fmt.Sprintf("%s/%s", a.gcsPrefix, endingJobRun.Name)
 	}
 	fmt.Printf("  starting from %v, ending at %q\n", query.StartOffset, query.EndOffset)
 

0c6e7e754fa03327270fe440e2b2175e41eef02c
Devan Goodwin <dgoodwin@redhat.com>
2023-06-19 09:30:45 -0300

    Add a mock for the GCSClient

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -2,3 +2,4 @@
 
 #https://github.com/golang/mock#gomock
 mockgen -build_flags=--mod=mod -destination=pkg/jobrunaggregator/jobrunaggregatorlib/ci_data_client_mock.go -package=jobrunaggregatorlib github.com/openshift/ci-tools/pkg/jobrunaggregator/jobrunaggregatorlib CIDataClient
+mockgen -build_flags=--mod=mod -destination=pkg/jobrunaggregator/jobrunaggregatorlib/gcs_client_mock.go -package=jobrunaggregatorlib github.com/openshift/ci-tools/pkg/jobrunaggregator/jobrunaggregatorlib CIGCSClient

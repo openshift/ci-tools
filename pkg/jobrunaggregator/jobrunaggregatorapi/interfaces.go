61a0cdce8026f713f58152882ea547893a50d74b
Devan Goodwin <dgoodwin@redhat.com>
2023-06-19 13:51:28 -0300

    Add mock impl for JobRunInfo

--- pkg/jobrunaggregator/jobrunaggregatorapi/interfaces.go
+++ pkg/jobrunaggregator/jobrunaggregatorapi/interfaces.go
@@ -13,7 +13,7 @@ import (
 )
 
 // JobRunInfo is a way to interact with JobRuns and gather their junit results.
-// The backing store can vary by impl, but GCS buckets and
+// The backing store can vary by impl, GCS buckets are the only implementation today.
 type JobRunInfo interface {
 	IsFinished(ctx context.Context) bool
 

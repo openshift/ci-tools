06262c9e7f9feb89800dc76470a337c00274a274
Forrest Babcock <fbabcock@redhat.com>
2023-06-22 13:38:33 -0400

    TRT-1091: Comment the test cases, minor updates

--- pkg/jobrunaggregator/jobrunaggregatoranalyzer/pass_fail.go
+++ pkg/jobrunaggregator/jobrunaggregatoranalyzer/pass_fail.go
@@ -446,12 +446,15 @@ func (a *weeklyAverageFromTenDays) CheckPercentileDisruption(ctx context.Context
 	return failureJobRunIDs, successJobRunIDs, testCaseFailed, messagePrefix + summary, nil
 }
 
-// evaluatePercentileDisruption performs a straight disruption > historicalThreshold comparison when no fuzzThreshold or multiplier are provided
-// when fuzzThreshold AND multiplier are present if the disruption is greater than the historicalThreshold
-// the difference between the disruption and historicalThreshold is multiplied by the multiplier and the result compared against the fuzzThreshold
-// the multiplier is the difference between the successful runs and the required successes so the closer we were to having the required successes the lower the bar for finding runs that are below the fuzzThreshold
-// the fuzzThreshold allows the fuzz factor to increase based on the historicalThreshold, currently 4 and under  = 1, 5-9 = 2, 10-14 = 3, etc.
-func evaluatePercentileDisruption(jobRunIDToAvailabilityResultForBackend map[string]jobrunaggregatorlib.AvailabilityResult, historicalThreshold, fuzzThreshold, multiplier float64) (failureJobRunIDs []string, successJobRunIDs []string, successRuns []string, failureRuns []string) {
+// evaluatePercentileDisruption performs a straight disruption > historicalThreshold comparison
+// when no fuzzThreshold or multiplier are provided. When fuzzThreshold AND multiplier are present
+// if the disruption is greater than the historicalThreshold the difference between the disruption
+// and historicalThreshold is multiplied by the multiplier and the result compared against the fuzzThreshold.
+// The multiplier is the difference between the successful runs and the required successes
+// so the closer we were to the required successes the lower the bar for finding runs that are below the fuzzThreshold.
+// The fuzzThreshold allows the fuzz factor to increase based on the historicalThreshold, currently 4 and under  = 1, 5-9 = 2, 10-14 = 3, etc.
+func evaluatePercentileDisruption(jobRunIDToAvailabilityResultForBackend map[string]jobrunaggregatorlib.AvailabilityResult, historicalThreshold,
+	fuzzThreshold, multiplier float64) (failureJobRunIDs []string, successJobRunIDs []string, successRuns []string, failureRuns []string) {
 	for jobRunID, disruption := range jobRunIDToAvailabilityResultForBackend {
 		disruptionValue := float64(disruption.SecondsUnavailable)
 		thresholdValue := historicalThreshold
@@ -518,8 +521,8 @@ func (a *weeklyAverageFromTenDays) checkPercentileDisruption(jobRunIDToAvailabil
 		// this will effectively change the comparison from straight disruption to comparing the fuzziness value
 		// fuzz is meant to increase the tolerance more as the historical disruption is larger, less than 5 fuzz == 1, 5 - 9 fuzz == 2, 10- 14 fuzz == 3, etc.
 		// we can tweak the fuzz factor by lowering / increasing the divisor
-		fuzz := (int64(threshold) / 5) + 1
-		failureJobRunIDs, successJobRunIDs, successRuns, failureRuns = evaluatePercentileDisruption(jobRunIDToAvailabilityResultForBackend, threshold, float64(fuzz), multiplier)
+		fuzzThreshold := (int64(threshold) / 5) + 1
+		failureJobRunIDs, successJobRunIDs, successRuns, failureRuns = evaluatePercentileDisruption(jobRunIDToAvailabilityResultForBackend, threshold, float64(fuzzThreshold), multiplier)
 
 		// revalidate, if numberOfPasses is ok now then we will fall though to the pass case
 		numberOfPasses = len(successJobRunIDs)
--- pkg/jobrunaggregator/jobrunaggregatoranalyzer/pass_fail_test.go
+++ pkg/jobrunaggregator/jobrunaggregatoranalyzer/pass_fail_test.go
@@ -31,6 +31,8 @@ func TestCheckPercentileDisruption(t *testing.T) {
 		successCount         int
 	}{
 		{
+			// Required Passes for 95th percentile is 6
+			// no disruption so it passes
 			name:                 "Test 95th Percentile 0's Pass",
 			disruptions:          []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 			thresholdPercentile:  95,
@@ -40,8 +42,13 @@ func TestCheckPercentileDisruption(t *testing.T) {
 			successCount:         10,
 		},
 		{
+			// Required Passes for 95th percentile is 6
+			// 5 Natural Passes
+			// Multiplier (6 - 5) == 1
+			// Fuzz Threshold ((historicalDisruption / 5) + 1) = 2
+			// The Disruption value that == 7 gets flipped to pass
 			name:                 "Test 95th Percentile Fuzzy Pass",
-			disruptions:          []int{5, 5, 5, 6, 7, 7, 8, 8, 9, 9},
+			disruptions:          []int{5, 5, 5, 6, 6, 7, 9, 9, 9, 9},
 			thresholdPercentile:  95,
 			historicalDisruption: 6,
 			status:               testCasePassed,
@@ -49,17 +56,28 @@ func TestCheckPercentileDisruption(t *testing.T) {
 			successCount:         6,
 		},
 		{
+			// Required Passes for 95th percentile is 6
+			// 4 Natural Passes
+			// Multiplier (6 - 4) == 2
+			// Fuzz Threshold ((historicalDisruption / 5) + 1) = 2
+			// The Disruption values that == 7 get flipped to passes
 			name:                 "Test 95th Percentile Multi Fuzzy Pass",
-			disruptions:          []int{5, 5, 5, 6, 7, 7, 7, 8, 8, 86},
+			disruptions:          []int{5, 5, 5, 6, 7, 7, 8, 8, 8, 86},
 			thresholdPercentile:  95,
 			historicalDisruption: 6,
 			status:               testCasePassed,
-			failedCount:          3,
-			successCount:         7,
+			failedCount:          4,
+			successCount:         6,
 		},
 		{
+			// Required Passes for 95th percentile is 6
+			// 4 Natural Passes
+			// Multiplier (6 - 4) == 2
+			// Fuzz Threshold ((historicalDisruption / 5) + 1) = 2
+			// The Disruption value that == 7 gets flipped to pass
+			// But the 8s and above do not, so we don't have enough passes
 			name:                 "Test 95th Percentile Multi Fuzzy Fail",
-			disruptions:          []int{5, 5, 6, 5, 7, 8, 8, 9, 9, 86},
+			disruptions:          []int{5, 5, 5, 6, 7, 8, 8, 9, 9, 86},
 			thresholdPercentile:  95,
 			historicalDisruption: 6,
 			status:               testCaseFailed,
@@ -67,6 +85,8 @@ func TestCheckPercentileDisruption(t *testing.T) {
 			successCount:         5,
 		},
 		{
+			// Required Passes for 80th percentile is 4
+			// 4 Natural Passes
 			name:                 "Test 80th Percentile Pass",
 			disruptions:          []int{0, 0, 1, 1, 2, 2, 2, 2, 2, 2},
 			thresholdPercentile:  80,
@@ -76,6 +96,11 @@ func TestCheckPercentileDisruption(t *testing.T) {
 			successCount:         4,
 		},
 		{
+			// Required Passes for 80th percentile is 4
+			// 3 Natural Passes
+			// Multiplier (4-3) == 1
+			// Fuzz Threshold ((historicalDisruption / 5) + 1) = 1
+			// The Disruption values that == 2 get flipped to passes
 			name:                 "Test 80th Percentile Xtra Fuzzy Pass",
 			disruptions:          []int{0, 0, 1, 2, 2, 6, 3, 4, 5, 8},
 			thresholdPercentile:  80,
@@ -85,6 +110,12 @@ func TestCheckPercentileDisruption(t *testing.T) {
 			successCount:         5,
 		},
 		{
+			// Required Passes for 80th percentile is 4
+			// 2 Natural Passes
+			// Multiplier (4 - 2) == 2
+			// Fuzz Threshold ((historicalDisruption / 5) + 1) = 1
+			// There are no disruption values that get flipped since (2-1)*2 = 2 and our Fuzz Threshold is 1
+			// But the 8s and above do not so we don't have enough passes
 			name:                 "Test 80th Percentile Fuzzy Fail",
 			disruptions:          []int{0, 0, 2, 2, 2, 2, 2, 2, 2, 2},
 			thresholdPercentile:  80,

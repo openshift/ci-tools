0e805afdff72c9c51f0ec0f223604a470558eda8
Stephen Goeddel <sgoeddel@redhat.com>
2023-07-31 09:17:09 -0400

    allow for promotion by tag in openshift-priv mirrored repos

--- cmd/ci-operator-config-mirror/main_test.go
+++ cmd/ci-operator-config-mirror/main_test.go
@@ -199,9 +199,14 @@ func TestPrivatePromotionConfiguration(t *testing.T) {
 		expected  *api.PromotionConfiguration
 	}{
 		{
-			id:        "changes expected",
+			id:        "promoted by name",
 			promotion: &api.PromotionConfiguration{Name: "4.x", Namespace: "ocp"},
 			expected:  &api.PromotionConfiguration{Name: "4.x-priv", Namespace: "ocp-private"},
+		},
+		{
+			id:        "promoted by tag",
+			promotion: &api.PromotionConfiguration{Tag: "4.x", Namespace: "ocp"},
+			expected:  &api.PromotionConfiguration{Tag: "4.x-priv", Namespace: "ocp-private"},
 		},
 	}
 	for _, tc := range testCases {

08a5acc2b48699732a704fae14544d6f31c04ff9
Steve Kuznetsov <skuznets@redhat.com>
2023-09-08 05:46:09 -0600

    Revert "Merge pull request #3608 from hongkailiu/revert-3602-skuznets/multiple-promotion-targets"
    This reverts commit 9a37c4b4c981d5c2ab97bfe41aebc9aa07fc2178, reversing
    changes made to 3e5e4e03e41e0bbafddf48532a0fb4dd1910f3e9.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- cmd/autopublicizeconfig/main.go
+++ cmd/autopublicizeconfig/main.go
@@ -187,7 +187,7 @@ func getReposForPrivateOrg(releaseRepoPath string, allowlist map[string][]string
 	}
 
 	callback := func(c *api.ReleaseBuildConfiguration, i *config.Info) error {
-		if !api.BuildsOfficialImages(c, api.WithoutOKD) {
+		if !api.BuildsAnyOfficialImages(c, api.WithoutOKD) {
 			return nil
 		}
 

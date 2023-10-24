802b1eb47608c246fac09f18ab878218e3289648
Bear <bechen@redhat.com>
2023-10-24 10:33:59 -0230

    Fix issue DPTP-3720 Add cluster profile gcp-3

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1263,6 +1263,7 @@ const (
 	ClusterProfileGCPQE                 ClusterProfile = "gcp-qe"
 	ClusterProfileGCPArm64              ClusterProfile = "gcp-arm64"
 	ClusterProfileGCP                   ClusterProfile = "gcp"
+	ClusterProfileGCP3                  ClusterProfile = "gcp-3"
 	ClusterProfileGCP40                 ClusterProfile = "gcp-40"
 	ClusterProfileGCPHA                 ClusterProfile = "gcp-ha"
 	ClusterProfileGCPCRIO               ClusterProfile = "gcp-crio"
@@ -1377,6 +1378,7 @@ func ClusterProfiles() []ClusterProfile {
 		ClusterProfileFleetManagerQE,
 		ClusterProfileGCP,
 		ClusterProfileGCP2,
+		ClusterProfileGCP3,
 		ClusterProfileGCP40,
 		ClusterProfileGCPCRIO,
 		ClusterProfileGCPHA,
@@ -1515,6 +1517,7 @@ func (p ClusterProfile) ClusterType() string {
 		ClusterProfileGCPQE,
 		ClusterProfileGCPArm64,
 		ClusterProfileGCP,
+		ClusterProfileGCP3,
 		ClusterProfileGCP40,
 		ClusterProfileGCPHA,
 		ClusterProfileGCPCRIO,
@@ -1719,6 +1722,8 @@ func (p ClusterProfile) LeaseType() string {
 		return "gcp-quota-slice"
 	case ClusterProfileGCP2:
 		return "gcp-openshift-gce-devel-ci-2-quota-slice"
+	case ClusterProfileGCP3:
+		return "gcp-3-quota-slice"
 	case ClusterProfileGCPVirtualization:
 		return "gcp-virtualization-quota-slice"
 	case ClusterProfileIBMCloud:

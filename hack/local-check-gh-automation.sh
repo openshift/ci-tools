1fdb07b16c208497c6dd2016be3bc8c892fc272c
Tomás Ó Dálaigh <tom1996.18@gmail.com>
2023-11-07 18:43:55 +0100

    resolved check-gh-bug

--- hack/local-check-gh-automation.sh
+++ hack/local-check-gh-automation.sh
@@ -2,6 +2,10 @@
 
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
+function OC() {
+	oc --context app.ci --namespace ci "$@"
+}
+
 function cleanup() {
   return_code=$?
   os::cleanup::all
@@ -13,9 +17,9 @@ trap "cleanup" EXIT
 data="${BASETMPDIR}/data"
 mkdir -p "${data}"
 
-function OC() {
-	oc --context app.ci --namespace ci "$@"
-}
+plugin_dir="${data}/plugins"
+mkdir -p "${plugin_dir}"
+OC extract configmap/plugins --to "${plugin_dir}"
 
 os::log::info "Extracting production data we need to run check-gh-automation..."
 OC extract secret/openshift-prow-github-app --keys appid,cert --to "${data}"
@@ -23,4 +27,7 @@ OC extract secret/openshift-prow-github-app --keys appid,cert --to "${data}"
 app_id=$(cat "${data}/appid")
 
 os::log::info "Running check-gh-automation"
-go run ./cmd/check-gh-automation --repo="$1" --bot=openshift-merge-robot --bot=openshift-ci-robot --github-app-id="$app_id" --github-app-private-key-path="${data}/cert"
+go run ./cmd/check-gh-automation --repo="$1" \
+  --bot=openshift-merge-robot --bot=openshift-ci-robot \
+  --github-app-id="$app_id" --github-app-private-key-path="${data}/cert" \
+  --plugin-config="${data}/plugins/plugins.yaml" --supplemental-plugin-config-dir="${data}/plugins" \

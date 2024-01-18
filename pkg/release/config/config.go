2a689117b1868745fb428cef979c73fa425396ab
Stephen Goeddel <sgoeddel@redhat.com>
2024-01-18 14:58:07 -0500

    add the ability to support multiple PRs to the payload-testing-prow-plugin

--- pkg/release/config/config.go
+++ pkg/release/config/config.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
+
 	"github.com/openshift/ci-tools/pkg/api"
 )
 
@@ -25,12 +29,34 @@ type ImageStreamRef struct {
 	ExcludeTags []string `json:"excludeTags,omitempty"`
 }
 
+// AdditionalPR is a formatted string that takes the form "org/repo#number"
+type AdditionalPR string
+
+const additionalPRPattern = `^([\w-]+)/([\w-]+)#(\d+)$`
+
+func (pr AdditionalPR) GetOrgRepoAndNumber() (string, string, int, error) {
+	re := regexp.MustCompile(additionalPRPattern)
+	matches := re.FindStringSubmatch(string(pr))
+	if len(matches) == 4 {
+		org := matches[1]
+		repo := matches[2]
+		number, err := strconv.Atoi(matches[3])
+		if err != nil {
+			return "", "", 0, fmt.Errorf("unable to get additional pr number from string: %s: %w", pr, err)
+		}
+		return org, repo, number, nil
+	} else {
+		return "", "", 0, fmt.Errorf("string: %s doesn't match expected format: org/repo#number", pr)
+	}
+}
+
 type Job struct {
 	Name                 string            `json:"name"`
 	Annotations          map[string]string `json:"annotations"`
 	api.MetadataWithTest `json:",inline"`
 
-	AggregatedCount int `json:"-"`
+	WithPRs         []AdditionalPR `json:"with-prs"`
+	AggregatedCount int            `json:"-"`
 }
 
 type AggregatedJob struct {

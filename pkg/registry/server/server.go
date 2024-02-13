631e43b9878d55767947e3a35a32c73ad381882d
Stephen Goeddel <sgoeddel@redhat.com>
2024-02-13 07:11:52 -0500

    guarantee unique metadata when merging configs from multiple sources

--- pkg/registry/server/server.go
+++ pkg/registry/server/server.go
@@ -10,6 +10,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/test-infra/prow/metrics"
 
 	"github.com/openshift/ci-tools/pkg/api"
@@ -451,7 +452,7 @@ func MetadataEntriesFromQuery(w http.ResponseWriter, r *http.Request) ([]api.Met
 		fmt.Fprint(w, "If any variants are passed, there must be one for each ref. Blank variants are allowed.")
 	}
 
-	var metadata []api.Metadata
+	metadata := make(sets.Set[api.Metadata])
 	for i, org := range orgs {
 		element := api.Metadata{
 			Org:    org,
@@ -461,8 +462,8 @@ func MetadataEntriesFromQuery(w http.ResponseWriter, r *http.Request) ([]api.Met
 		if variantsExist {
 			element.Variant = variants[i]
 		}
-		metadata = append(metadata, element)
+		metadata.Insert(element)
 	}
 
-	return metadata, nil
+	return metadata.UnsortedList(), nil
 }

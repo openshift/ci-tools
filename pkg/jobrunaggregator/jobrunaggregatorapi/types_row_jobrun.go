15d2a9d70c9bae2ff8571e009c5eee41e6b9b55d
Devan Goodwin <dgoodwin@redhat.com>
2024-04-29 14:46:40 -0300

    Remove dead view creation code
    This was dangerous, out of date, missing for many tables and views. Schema management would be nice but it needs to be done properly and fully to serve any value.

--- pkg/jobrunaggregator/jobrunaggregatorapi/types_row_jobrun.go
+++ pkg/jobrunaggregator/jobrunaggregatorapi/types_row_jobrun.go
@@ -63,26 +63,6 @@ const (
   }
 ]
 `
-
-	// used for TestRuns_Unified_AllJobRuns
-	testRunsUnifiedJobRunsSchema = `
-SELECT 
-  JobRuns.name as JobRunName,
-  Jobs.Jobname as JobName,
-  JobRuns.StartTime as JobRunStartTime,
-  JobRuns.ReleaseTag as ReleaseTag,
-  JobRuns.Cluster as Cluster,
-  Jobs.Platform as Platform,
-  Jobs.Architecture as Architecture,
-  Jobs.Network as Network,
-  Jobs.IPMode as IPMode,
-  Jobs.Topology as Topology,
-  Jobs.Release as Release,
-  Jobs.FromRelease as FromRelease,
-  if(Jobs.FromRelease="",false,true) as IsUpgrade,
-FROM openshift-ci-data-analysis.ci_data.JobRuns
-INNER JOIN openshift-ci-data-analysis.ci_data.Jobs on JobRuns.JobName = Jobs.JobName
-`
 )
 
 type JobRunRow struct {

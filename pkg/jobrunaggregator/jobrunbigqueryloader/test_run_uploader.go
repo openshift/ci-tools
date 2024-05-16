2e31ca598ffe40a935f941e23e4e57f15aa0f316
Devan Goodwin <dgoodwin@redhat.com>
2024-05-16 13:39:44 -0300

    Comment fix up

--- pkg/jobrunaggregator/jobrunbigqueryloader/test_run_uploader.go
+++ pkg/jobrunaggregator/jobrunbigqueryloader/test_run_uploader.go
@@ -7,8 +7,9 @@ import (
 	"github.com/openshift/ci-tools/pkg/jobrunaggregator/jobrunaggregatorlib"
 )
 
-// pendingJobRunsUploadLister is used to find out which job runs are new for an uploader. Each uploader has
-// its own job runs table allowing for independent execution.
+// pendingJobRunsUploadLister is used to find out which job runs are new for an uploader.
+// JobRun data is denormalized with every row in Alerts / BackendDisruption, and thus we query recent
+// uploaded job runs by examining these columns.
 type pendingJobRunsUploadLister struct {
 	tableName    string
 	ciDataClient jobrunaggregatorlib.CIDataClient

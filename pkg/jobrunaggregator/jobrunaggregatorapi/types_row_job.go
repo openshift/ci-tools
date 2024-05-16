50384cee3c7cf882718b60e2dba09540cfe2da84
Devan Goodwin <dgoodwin@redhat.com>
2024-05-16 08:15:25 -0300

    Remove more dead table management in bigquery

--- pkg/jobrunaggregator/jobrunaggregatorapi/types_row_job.go
+++ pkg/jobrunaggregator/jobrunaggregatorapi/types_row_job.go
@@ -2,43 +2,8 @@ package jobrunaggregatorapi
 
 import "cloud.google.com/go/bigquery"
 
-// The jobSchema below is used to build the "Jobs" table.
 const (
 	JobsTableName = "Jobs"
-	JobSchema     = `
-[
-  {
-    "name": "JobName",
-    "description": "name of the job from CI",
-    "type": "STRING",
-    "mode": "REQUIRED"
-  },
-  {
-    "name": "GCSBucketName",
-    "description": "name of the GCS bucket we store jobrun artifacts in",
-    "type": "STRING",
-    "mode": "REQUIRED"
-  },
-  {
-    "name": "GCSJobHistoryLocationPrefix",
-    "description": "spot in the GCS bucket to look.  like logs/<jobName>",
-    "type": "STRING",
-    "mode": "REQUIRED"
-  },
-  {
-    "name": "CollectDisruption",
-    "description": "should we collect disruption data from the job runs",
-    "type": "BOOLEAN",
-    "mode": "REQUIRED"
-  },
-  {
-    "name": "CollectTestRuns",
-    "description": "should we collect test run data from the job runs",
-    "type": "BOOLEAN",
-    "mode": "REQUIRED"
-  }
-]
-`
 )
 
 type JobRow struct {
--- pkg/jobrunaggregator/jobrunaggregatorapi/types_row_jobrun.go
+++ pkg/jobrunaggregator/jobrunaggregatorapi/types_row_jobrun.go
@@ -13,59 +13,6 @@ const (
 	// TODO: Remove population of this table, I don't think it's used anywhere in ci-tools or the views in bigquery.
 	// Instead used the data embedded in each Alerts row, or the jobs table in openshift-gce-devel.ci_analysis_us
 	AlertJobRunTableName = "Alerts_JobRuns"
-
-	JobRunSchema = `
-[
-  {
-    "name": "Name",
-    "description": "name of the jobrun (the long number)",
-    "type": "STRING",
-    "mode": "REQUIRED"
-  },
-  {
-    "name": "JobName",
-    "description": "name of the job from CI",
-    "type": "STRING",
-    "mode": "REQUIRED"
-  },
-  {
-    "name": "Status",
-    "description": "error, failure, success",
-    "type": "STRING",
-    "mode": "NULLABLE"
-  },
-  {
-    "name": "StartTime",
-    "description": "time the jobrun started",
-    "type": "TIMESTAMP",
-    "mode": "NULLABLE"
-  },
-  {
-    "name": "EndTime",
-    "description": "time the jobrun started",
-    "type": "TIMESTAMP",
-    "mode": "NULLABLE"
-  },
-  {
-    "name": "ReleaseTag",
-    "description": "",
-    "type": "STRING",
-    "mode": "NULLABLE"
-  },
-  {
-    "name": "Cluster",
-    "description": "the build farm cluster that the CI job ran on: build01, build02, build03, vsphere, etc",
-    "type": "STRING",
-    "mode": "NULLABLE"
-  },
-  {
-    "name": "MasterNodesUpdated",
-    "description": "indicator if master nodes restarted during the jobrun",
-    "type": "STRING",
-    "mode": "NULLABLE"
-  }
-]
-`
 )
 
 type JobRunRow struct {

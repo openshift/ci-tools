45e441c15d7f99b50603cdd9b84007a0b05e6dd7
Danilo Gemoli <dgemoli@redhat.com>
2024-07-11 12:27:31 +0200

    chore: make verify-gen

--- cmd/ci-operator/main.go
+++ cmd/ci-operator/main.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/bombsimon/logrusr/v3"
 	"github.com/go-logr/logr"
+	egressfirewallv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1"
 	"github.com/sirupsen/logrus"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -86,8 +87,6 @@ import (
 	"github.com/openshift/ci-tools/pkg/util"
 	"github.com/openshift/ci-tools/pkg/util/gzip"
 	"github.com/openshift/ci-tools/pkg/validation"
-
-	egressfirewallv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1"
 )
 
 const usage = `Orchestrate multi-stage image-based builds

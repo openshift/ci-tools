64e65c67560f80d7da2bf3075c5456114d8fd927
Danilo Gemoli <dgemoli@redhat.com>
2024-08-21 20:59:00 +0200

    feat(cluster-init): add aws provion create-stacks

--- cmd/cluster-init/cmd/provision.go
+++ cmd/cluster-init/cmd/provision.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/openshift/ci-tools/pkg/clustermgmt"
+	"github.com/openshift/ci-tools/pkg/clustermgmt/provision/aws"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -31,6 +35,10 @@ func newProvisionAWS(ctx context.Context, log *logrus.Entry) *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "aws",
 		Short: "Provision assets on AWS",
+		Long: `Provision the required infrastructure on AWS.
+An AWS profile must be properly set for these subcommands to work properly.
+How to use a named profile: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html#cli-configure-files-using-profiles
+For more information regarding env. variables: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html#envvars-set`,
 	}
 	cmd.AddCommand(newAWSCreateStacks(ctx, log))
 	return &cmd
@@ -40,8 +48,23 @@ func newAWSCreateStacks(ctx context.Context, log *logrus.Entry) *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "create-stacks",
 		Short: "Create cloud formation stacks",
+		Long:  `Create cloud formation stacks `,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			step := newCreateAWSStacksStep(log, opts.clusterInstall)
+			step := aws.NewCreateAWSStacksStep(log,
+				func() (*clustermgmt.ClusterInstall, error) {
+					log.WithField("path", opts.clusterInstall).Info("Loading cluster install")
+					return clustermgmt.LoadClusterInstall(opts.clusterInstall)
+				},
+				func() (aws.CloudFormationClient, error) {
+					log.Info("Loading AWS config")
+					awsconfig, err := config.LoadDefaultConfig(ctx)
+					if err != nil {
+						return nil, fmt.Errorf("load aws config: %v", err)
+					}
+					return cloudformation.NewFromConfig(awsconfig), nil
+				},
+				nil, nil,
+			)
 			if err := step.Run(ctx); err != nil {
 				return fmt.Errorf("%s: %v", step.Name(), err)
 			}
@@ -49,22 +72,4 @@ func newAWSCreateStacks(ctx context.Context, log *logrus.Entry) *cobra.Command {
 		},
 	}
 	return &cmd
-}
-
-type createAWSStacksStep struct {
-	log            *logrus.Entry
-	clusterInstall string
-}
-
-func (s *createAWSStacksStep) Run(ctx context.Context) error {
-	s.log.Info("create AWS stacks")
-	return nil
-}
-
-func (s *createAWSStacksStep) Name() string {
-	return "create-aws-stacks"
-}
-
-func newCreateAWSStacksStep(log *logrus.Entry, clusterInstall string) *createAWSStacksStep {
-	return &createAWSStacksStep{log: log, clusterInstall: clusterInstall}
 }
---
+++ pkg/clustermgmt/provision/aws/createstacks.go
@@ -0,0 +1,160 @@
+package aws
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	cloudformationtypes "github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+	"github.com/openshift/ci-tools/pkg/clustermgmt"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	createStackCompleteWaitTimeDef time.Duration = 10 * time.Minute
+)
+
+// CloudFormationClient is a conveniente interface that has been created
+// to make unit test easier to write
+type CloudFormationClient interface {
+	CreateStack(ctx context.Context, params *cloudformation.CreateStackInput, optFns ...func(*cloudformation.Options)) (*cloudformation.CreateStackOutput, error)
+	DescribeStacks(context.Context, *cloudformation.DescribeStacksInput, ...func(*cloudformation.Options)) (*cloudformation.DescribeStacksOutput, error)
+}
+
+type CloudFormationClientGetter func() (CloudFormationClient, error)
+type TemplateResolver func(path string) (string, error)
+
+type createAWSStacksStep struct {
+	log                         *logrus.Entry
+	getClusterInstall           clustermgmt.ClusterInstallGetter
+	getCFClient                 CloudFormationClientGetter
+	createStackCompleteWaitTime *time.Duration
+	templateResolver            TemplateResolver
+}
+
+func (s *createAWSStacksStep) Name() string {
+	return "create-aws-stacks"
+}
+
+func (s *createAWSStacksStep) Run(ctx context.Context) error {
+	log := s.log.WithField("step", "provision: aws: create stacks")
+
+	ci, err := s.getClusterInstall()
+	if err != nil {
+		return fmt.Errorf("get cluster install: %v", err)
+	}
+
+	if ci.Provision.AWS == nil {
+		log.Info("No AWS provision stanza")
+		return nil
+	}
+	if len(ci.Provision.AWS.CloudFormationTemplates) == 0 {
+		log.Info("No cloud formations templates stanza")
+		return nil
+	}
+
+	client, err := s.getCFClient()
+	if err != nil {
+		return fmt.Errorf("get cloud formation client: %v", err)
+	}
+
+	if err := s.createStacks(ctx, log, client, ci.Provision.AWS.CloudFormationTemplates); err != nil {
+		return err
+	}
+	if err := waitForStacksToComplete(ctx, log, client, ci.Provision.AWS.CloudFormationTemplates, *s.createStackCompleteWaitTime); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *createAWSStacksStep) createStacks(ctx context.Context,
+	log *logrus.Entry,
+	client CloudFormationClient,
+	templates []clustermgmt.AWSCloudFormationTemplate) error {
+	for _, t := range templates {
+		log := log.WithField("stack", t.StackName)
+
+		params := make([]cloudformationtypes.Parameter, len(t.Parameters))
+		for i, p := range t.Parameters {
+			params[i] = cloudformationtypes.Parameter{ParameterKey: &p.Key, ParameterValue: &p.Value}
+		}
+		capabilities := make([]cloudformationtypes.Capability, len(t.Capabilities))
+		for i, s := range t.Capabilities {
+			capabilities[i] = cloudformationtypes.Capability(s)
+		}
+
+		templateBody, err := s.templateResolver(t.TemplateBody)
+		if err != nil {
+			return fmt.Errorf("read template body file %s: %v", t.TemplateBody, err)
+		}
+
+		log.Info("Creating stack")
+		_, err = client.CreateStack(ctx, &cloudformation.CreateStackInput{
+			StackName:    &t.StackName,
+			TemplateBody: aws.String(templateBody),
+			Parameters:   params,
+			Capabilities: capabilities,
+		})
+
+		if err != nil {
+			aee := &cloudformationtypes.AlreadyExistsException{}
+			if errors.As(err, &aee) {
+				log.Warn("Stack exists already, skipping")
+			} else {
+				return fmt.Errorf("create stack %s: %v", t.StackName, err)
+			}
+		}
+	}
+	return nil
+}
+
+func waitForStacksToComplete(ctx context.Context, log *logrus.Entry, client CloudFormationClient,
+	templates []clustermgmt.AWSCloudFormationTemplate, wait time.Duration) error {
+	waiter := cloudformation.NewStackCreateCompleteWaiter(client)
+	waiters, wCtx := errgroup.WithContext(ctx)
+	waiters.SetLimit(len(templates))
+	for _, t := range templates {
+		waiters.Go(func() error {
+			log := log.WithField("stack", t.StackName)
+			log.Info("Waiting to complete")
+			if err := waiter.Wait(wCtx, &cloudformation.DescribeStacksInput{StackName: &t.StackName},
+				wait); err != nil {
+				return fmt.Errorf("stack %s failed: %v", t.StackName, err)
+			}
+			log.Info("Created successfully")
+			return nil
+		})
+	}
+	return waiters.Wait()
+}
+
+func resolveTemplate(path string) (string, error) {
+	t, err := os.ReadFile(path)
+	return string(t), err
+}
+
+func NewCreateAWSStacksStep(log *logrus.Entry,
+	getClusterInstall clustermgmt.ClusterInstallGetter,
+	getCFClient CloudFormationClientGetter,
+	createStackCompleteWaitTime *time.Duration,
+	templateResolver TemplateResolver) *createAWSStacksStep {
+	if createStackCompleteWaitTime == nil {
+		createStackCompleteWaitTime = &createStackCompleteWaitTimeDef
+	}
+	if templateResolver == nil {
+		templateResolver = resolveTemplate
+	}
+	return &createAWSStacksStep{
+		log:                         log,
+		getClusterInstall:           getClusterInstall,
+		getCFClient:                 getCFClient,
+		createStackCompleteWaitTime: createStackCompleteWaitTime,
+		templateResolver:            templateResolver,
+	}
+}

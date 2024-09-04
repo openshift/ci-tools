7ce2067618dc5257754d12affdeeac99bcf8a66d
Danilo Gemoli <dgemoli@redhat.com>
2024-09-04 14:42:56 +0200

    feat(cluster-init): add integration tests

--- test/integration/cluster-init.sh
+++ test/integration/cluster-init.sh
@@ -25,5 +25,15 @@ actual_update="${tempdir}/update/input"
 expected_update="${suite_dir}/update/expected"
 os::cmd::expect_success "cluster-init onboard config generate --cluster-name=existingCluster --release-repo=${actual_update} --update=true --create-pr=false"
 os::integration::compare "${actual_update}" "${expected_update}"
+# test the create ocp scenario
+actual_create="${tempdir}/create-ocp/input"
+expected_create="${suite_dir}/create-ocp/expected"
+os::cmd::expect_success "cluster-init onboard config generate --hosted --cluster-name=newCluster --release-repo=${actual_create} --create-pr=false --osd=false"
+os::integration::compare "${actual_create}" "${expected_create}"
+# test the update scenario
+actual_update="${tempdir}/update-ocp/input"
+expected_update="${suite_dir}/update-ocp/expected"
+os::cmd::expect_success "cluster-init onboard config generate --cluster-name=existingCluster --release-repo=${actual_update} --update=true --create-pr=false --osd=false"
+os::integration::compare "${actual_update}" "${expected_update}"
 
 os::test::junit::declare_suite_end

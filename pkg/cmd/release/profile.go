102698af827b902cc9ee8db414a144f2723fece8
Patrícia Salajová <72410580+psalajova@users.noreply.github.com>
2024-10-29 15:10:48 +0100

    Remove cluster profile secrets from ci tools (& from `prowgen`) (#4261)
    * Comment out Secret() from release tool
    
    * Prowgen: remove functions related to creating cluster profile volume(mount)s
    
    * Update unit tests
    
    * Remove the Secret() function
    
    * Update check-cluster-profiles-config to fetch info through config-resolver
    
    * Update prowgen integration tests: remove cluster-profile fields from expected output
    
    * Remove templates integration test
    
    * Update pj-rehearse integration tests
    
    * Update expected.yaml
    
    ---------
    
    Co-authored-by: test <test>

--- pkg/cmd/release/profile.go
+++ pkg/cmd/release/profile.go
@@ -54,8 +54,8 @@ func profilePrint(args []string) error {
 			Profile:     p,
 			ClusterType: p.ClusterType(),
 			LeaseType:   p.LeaseType(),
-			Secret:      p.Secret(),
-			ConfigMap:   p.ConfigMap(),
+			//Secret:      p.Secret(),
+			ConfigMap: p.ConfigMap(),
 		})
 	}
 	return printYAML(l)

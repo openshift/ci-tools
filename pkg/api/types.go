2ac16926f6830fa27c6890eaa6b6ba97c28203f7
Yash Ajgaonkar <yajgaonk@redhat.com>
2025-03-03 17:12:02 +0530

    Add GCP sustaining cluster profile

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1335,6 +1335,7 @@ const (
 	ClusterProfileGCPTelco                ClusterProfile = "gcp-telco"
 	ClusterProfileGCPChaos                ClusterProfile = "gcp-chaos"
 	ClusterProfileIBMCloud                ClusterProfile = "ibmcloud"
+	ClusterProfileGCPSustAutoRel412       ClusterProfile = "gcp-sustaining-autorelease-412"
 	ClusterProfileIBMCloudCSPIQE          ClusterProfile = "ibmcloud-cspi-qe"
 	ClusterProfileIBMCloudRHOAIQE         ClusterProfile = "ibmcloud-rhoai-qe"
 	ClusterProfileIBMCloudQE              ClusterProfile = "ibmcloud-qe"
@@ -1495,6 +1496,7 @@ func ClusterProfiles() []ClusterProfile {
 		ClusterProfileGCPOpendatahub,
 		ClusterProfileGCPTelco,
 		ClusterProfileGCPChaos,
+		ClusterProfileGCPSustAutoRel412,
 		ClusterProfileAWSVirtualization,
 		ClusterProfileAzureVirtualization,
 		ClusterProfileHyperShift,
@@ -1692,6 +1694,7 @@ func (p ClusterProfile) ClusterType() string {
 		ClusterProfileGCP3,
 		ClusterProfileGCP2,
 		ClusterProfileGCPVirtualization,
+		ClusterProfileGCPSustAutoRel412,
 		ClusterProfileGCPOpendatahub,
 		ClusterProfileGCPTelco,
 		ClusterProfileGCPChaos,
@@ -1965,6 +1968,8 @@ func (p ClusterProfile) LeaseType() string {
 		return "gcp-opendatahub-quota-slice"
 	case ClusterProfileGCPTelco:
 		return "gcp-telco-quota-slice"
+	case ClusterProfileGCPSustAutoRel412:
+		return "gcp-sustaining-autorelease-412-quota-slice"
 	case ClusterProfileGCPChaos:
 		return "gcp-chaos-quota-slice"
 	case ClusterProfileIBMCloud:

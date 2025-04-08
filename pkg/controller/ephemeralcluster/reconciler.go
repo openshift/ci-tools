4f5924b18a08ceb2bc05409e2dd7d9945749213b
Danilo Gemoli <dgemoli@redhat.com>
2025-04-08 12:04:36 +0200

    feat(ecc): fetch kubeconfig

--- pkg/controller/ephemeralcluster/reconciler.go
+++ pkg/controller/ephemeralcluster/reconciler.go
@@ -27,9 +27,11 @@ import (
 	"github.com/openshift/ci-tools/pkg/api"
 	ephemeralclusterv1 "github.com/openshift/ci-tools/pkg/api/ephemeralcluster/v1"
 	"github.com/openshift/ci-tools/pkg/prowgen"
+	"github.com/openshift/ci-tools/pkg/steps"
 )
 
 const (
+	WaitTestStepName = "wait-test-complete"
 	ProwJobNamespace = "ci"
 )
 
@@ -51,7 +53,8 @@ type reconciler struct {
 	configUploader ConfigSpecUploader
 
 	// Mock for testing
-	now func() time.Time
+	now     func() time.Time
+	polling func() time.Duration
 }
 
 func AddToManager(logger *logrus.Entry, mgr manager.Manager, allManagers map[string]manager.Manager) error {
@@ -77,7 +80,7 @@ func AddToManager(logger *logrus.Entry, mgr manager.Manager, allManagers map[str
 }
 
 func (r *reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
-	logger := r.logger.WithField("request", req.String())
+	log := r.logger.WithField("request", req.String())
 
 	ec := &ephemeralclusterv1.EphemeralCluster{}
 	if err := r.masterClient.Get(ctx, req.NamespacedName, ec); err != nil {
@@ -98,16 +101,20 @@ func (r *reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reco
 			return r.handleGetProwJobError(err)
 		}
 	} else {
-		r.logger.Info("ProwJob not found, creating")
-		r.createProwJob(ctx, ec)
-		err := r.masterClient.Update(ctx, ec)
-		if err != nil {
-			err = fmt.Errorf("update ephemeral cluster: %w", err)
-		}
-		return reconcile.Result{}, err
+		log.Info("ProwJob not found, creating")
+		r.createProwJob(ctx, log, ec)
+		return reconcile.Result{RequeueAfter: r.polling()}, r.updateEphemeralCluster(ctx, ec)
 	}
 
-	logger.Info("Finished reconciliation")
+	if ec.Status.Kubeconfig == "" {
+		log.Info("Fetching the kubeconfig")
+		res, err := r.fetchKubeconfig(ctx, log, ec, &pj)
+		if err := r.updateEphemeralCluster(ctx, ec); err != nil {
+			return reconcile.Result{RequeueAfter: r.polling()}, err
+		}
+		return res, err
+	}
+
 	return reconcile.Result{}, nil
 }
 
@@ -120,7 +127,7 @@ func (r *reconciler) handleGetProwJobError(err error) (reconcile.Result, error) 
 	}
 }
 
-func (r *reconciler) createProwJob(ctx context.Context, ec *ephemeralclusterv1.EphemeralCluster) {
+func (r *reconciler) createProwJob(ctx context.Context, log *logrus.Entry, ec *ephemeralclusterv1.EphemeralCluster) {
 	ciOperatorConfig := &api.ReleaseBuildConfiguration{
 		InputConfiguration: api.InputConfiguration{
 			Releases: map[string]api.UnresolvedRelease{
@@ -138,7 +145,7 @@ func (r *reconciler) createProwJob(ctx context.Context, ec *ephemeralclusterv1.E
 			As: "cluster-provisioning",
 			MultiStageTestConfigurationLiteral: &api.MultiStageTestConfigurationLiteral{
 				Test: []api.LiteralTestStep{{
-					As:       "wait-test-complete",
+					As:       WaitTestStepName,
 					From:     "cli",
 					Commands: waitKubeconfigSh,
 					Resources: api.ResourceRequirements{
@@ -157,20 +164,20 @@ func (r *reconciler) createProwJob(ctx context.Context, ec *ephemeralclusterv1.E
 
 	pj, err := r.makeProwJob(ciOperatorConfig)
 	if err != nil {
-		upsertProvisioningCond(ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.CIOperatorJobsGenerateFailure, err.Error())
+		upsertProvisioningCond(ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.CIOperatorJobsGenerateFailureReason, err.Error())
 		return
 	}
 
 	location, err := r.uploadCIOperatorConfig(ctx, ciOperatorConfig)
 	if err != nil {
 		err := fmt.Errorf("upload ci config: %w", err)
-		upsertProvisioningCond(ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.CIOperatorJobsGenerateFailure, err.Error())
+		upsertProvisioningCond(ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.CIOperatorJobsGenerateFailureReason, err.Error())
 		return
 	}
-	r.logger.WithField("Path", location).Info("Config uploaded to GCS")
+	log.WithField("Path", location).Info("Config uploaded to GCS")
 
 	if len(pj.Spec.PodSpec.Containers) != 1 {
-		upsertProvisioningCond(ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.CIOperatorJobsGenerateFailure, "too many presubmit containers")
+		upsertProvisioningCond(ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.CIOperatorJobsGenerateFailureReason, "too many presubmit containers")
 		return
 	}
 	container := &pj.Spec.PodSpec.Containers[0]
@@ -178,7 +185,7 @@ func (r *reconciler) createProwJob(ctx context.Context, ec *ephemeralclusterv1.E
 
 	if err := r.masterClient.Create(ctx, pj); err != nil {
 		err = fmt.Errorf("create prowjob: %w", err)
-		upsertProvisioningCond(ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.CIOperatorJobsGenerateFailure, err.Error())
+		upsertProvisioningCond(ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.CIOperatorJobsGenerateFailureReason, err.Error())
 		return
 	}
 
@@ -230,6 +237,56 @@ func (r *reconciler) uploadCIOperatorConfig(ctx context.Context, config *api.Rel
 		return "", fmt.Errorf("marshal config: %w", err)
 	}
 	return r.configUploader.UploadConfigSpec(ctx, gcsPath, string(configBytes))
+}
+
+func (r *reconciler) fetchKubeconfig(ctx context.Context, log *logrus.Entry, ec *ephemeralclusterv1.EphemeralCluster, pj *prowv1.ProwJob) (reconcile.Result, error) {
+	buildClient, ok := r.buildClients[pj.Spec.Cluster]
+	if !ok {
+		err := fmt.Errorf("unkown cluster %s", pj.Spec.Cluster)
+		r.upsertCondition(ec, ephemeralclusterv1.ClusterReady, ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.KubeconfigFetchFailureReason, err.Error())
+		return reconcile.Result{}, reconcile.TerminalError(err)
+	}
+
+	nss := corev1.NamespaceList{}
+	if err := buildClient.List(ctx, &nss, ctrlruntimeclient.MatchingLabels{steps.LabelJobID: pj.Name}); err != nil {
+		err := fmt.Errorf("get namespace for %s: %w", pj.Name, err)
+		r.upsertCondition(ec, ephemeralclusterv1.ClusterReady, ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.KubeconfigFetchFailureReason, err.Error())
+		return reconcile.Result{RequeueAfter: r.polling()}, nil
+	}
+
+	// The NS hasn't been created yet, requeuing.
+	if len(nss.Items) == 0 {
+		r.upsertCondition(ec, ephemeralclusterv1.ClusterReady, ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.KubeconfigFetchFailureReason, ephemeralclusterv1.CIOperatorNSNotFoundMsg)
+		return reconcile.Result{RequeueAfter: r.polling()}, nil
+	}
+
+	ns := nss.Items[0].Name
+	kubeconfigSecret := corev1.Secret{}
+	// The secret is named after the test step name.
+	if err := buildClient.Get(ctx, types.NamespacedName{Name: WaitTestStepName, Namespace: ns}, &kubeconfigSecret); err != nil {
+		r.upsertCondition(ec, ephemeralclusterv1.ClusterReady, ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.KubeconfigFetchFailureReason, err.Error())
+		return reconcile.Result{RequeueAfter: r.polling()}, nil
+	}
+
+	kubeconfig, ok := kubeconfigSecret.Data["kubeconfig"]
+	if !ok {
+		// The kubeconfig takes time before being stored into the secret, requeuing.
+		r.upsertCondition(ec, ephemeralclusterv1.ClusterReady, ephemeralclusterv1.ConditionFalse, ephemeralclusterv1.KubeconfigFetchFailureReason, ephemeralclusterv1.KubeconfigNotReadMsg)
+		return reconcile.Result{RequeueAfter: r.polling()}, nil
+	}
+
+	ec.Status.Kubeconfig = string(kubeconfig)
+	log.Info("kubeconfig fetched")
+	r.upsertCondition(ec, ephemeralclusterv1.ClusterReady, ephemeralclusterv1.ConditionTrue, "", "")
+
+	return reconcile.Result{}, nil
+}
+
+func (r *reconciler) updateEphemeralCluster(ctx context.Context, ec *ephemeralclusterv1.EphemeralCluster) error {
+	if err := r.masterClient.Update(ctx, ec); err != nil {
+		return fmt.Errorf("update ephemereal cluster: %w", err)
+	}
+	return nil
 }
 
 func (r *reconciler) upsertCondition(ec *ephemeralclusterv1.EphemeralCluster, t ephemeralclusterv1.EphemeralClusterConditionType, status ephemeralclusterv1.ConditionStatus, reason, msg string) {

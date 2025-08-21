8cf9a4bd0f205ee9ace0622f136181589cf3cca2
Jakub Guzik <jguzik@redhat.com>
2025-08-21 16:31:26 +0200

    Upgrades to frequency-reducer: improved logic, multithreading, added cluster profiles
    Signed-off-by: Jakub Guzik <jguzik@redhat.com>

--- cmd/branchingconfigmanagers/frequency-reducer/main.go
+++ cmd/branchingconfigmanagers/frequency-reducer/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"os"
 	"regexp"
+	"runtime"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/robfig/cron.v2"
+	"gopkg.in/yaml.v2"
 
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 
@@ -18,15 +23,25 @@ import (
 	"github.com/openshift/ci-tools/pkg/config"
 )
 
+// ClusterProfilesConfig defines the YAML structure for cluster profiles filtering
+type ClusterProfilesConfig struct {
+	ClusterProfiles []string `yaml:"cluster_profiles"`
+}
+
 type options struct {
 	config.ConfirmableOptions
-	currentOCPVersion string
+	currentOCPVersion     string
+	maxThreads            int
+	clusterProfilesConfig string
 }
 
 func (o options) validate() error {
 	var errs []error
 	if err := o.ConfirmableOptions.Validate(); err != nil {
 		errs = append(errs, err)
+	}
+	if o.maxThreads <= 0 {
+		errs = append(errs, fmt.Errorf("max-threads must be positive, got %d", o.maxThreads))
 	}
 
 	return utilerrors.NewAggregate(errs)
@@ -35,6 +50,8 @@ func (o options) validate() error {
 func gatherOptions() options {
 	o := options{}
 	flag.StringVar(&o.currentOCPVersion, "current-release", "", "Current OCP version")
+	flag.IntVar(&o.maxThreads, "max-threads", runtime.NumCPU(), "Maximum number of threads to use for parallel processing")
+	flag.StringVar(&o.clusterProfilesConfig, "cluster-profiles-config", "", "Path to YAML file containing cluster profiles to filter by (optional)")
 
 	o.Bind(flag.CommandLine)
 	flag.Parse()
@@ -57,43 +74,228 @@ func main() {
 		logrus.Fatalf("Couldn't complete the config options: %v", err)
 	}
 
-	if err := o.OperateOnCIOperatorConfigDir(o.ConfigDir, func(configuration *api.ReleaseBuildConfiguration, info *config.Info) error {
-		output := config.DataWithInfo{Configuration: *configuration, Info: *info}
-		updateIntervalFieldsForMatchedSteps(&output, *ocpVersion)
-
-		if err := output.CommitTo(o.ConfigDir); err != nil {
-			logrus.WithError(err).Fatal("commitTo failed")
+	// Load cluster profiles filter if provided
+	var allowedClusterProfiles map[string]bool
+	if o.clusterProfilesConfig != "" {
+		var err error
+		allowedClusterProfiles, err = loadClusterProfilesConfig(o.clusterProfilesConfig)
+		if err != nil {
+			logrus.WithError(err).Fatal("Could not load cluster profiles configuration.")
 		}
-		return nil
-	}); err != nil {
-		logrus.WithError(err).Fatal("Could not branch configurations.")
+		logrus.Infof("Loaded cluster profiles filter: %d profiles specified", len(allowedClusterProfiles))
+	} else {
+		logrus.Info("No cluster profiles filter specified, processing all configurations")
 	}
 
+	if err := processConfigurationsInParallel(&o, *ocpVersion, allowedClusterProfiles); err != nil {
+		logrus.WithError(err).Fatal("Could not process configurations.")
+	}
+
+}
+
+// loadClusterProfilesConfig loads and parses the cluster profiles configuration file
+func loadClusterProfilesConfig(filePath string) (map[string]bool, error) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return nil, fmt.Errorf("cluster profiles config file does not exist: %s", filePath)
+	}
+
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read cluster profiles config file: %w", err)
+	}
+
+	var config ClusterProfilesConfig
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("failed to parse cluster profiles config YAML: %w", err)
+	}
+
+	if len(config.ClusterProfiles) == 0 {
+		return nil, fmt.Errorf("no cluster profiles specified in config file")
+	}
+
+	// Convert to map for O(1) lookup
+	allowedProfiles := make(map[string]bool)
+	for _, profile := range config.ClusterProfiles {
+		allowedProfiles[profile] = true
+		logrus.Debugf("Allowing cluster profile: %s", profile)
+	}
+
+	return allowedProfiles, nil
+}
+
+type configJob struct {
+	configuration *api.ReleaseBuildConfiguration
+	info          *config.Info
+	configDir     string
+}
+
+func processConfigurationsInParallel(o *options, ocpVersion ocplifecycle.MajorMinor, allowedClusterProfiles map[string]bool) error {
+	var jobs []configJob
+	var jobsMutex sync.Mutex
+
+	err := o.OperateOnCIOperatorConfigDir(o.ConfigDir, func(configuration *api.ReleaseBuildConfiguration, info *config.Info) error {
+		jobsMutex.Lock()
+		jobs = append(jobs, configJob{
+			configuration: configuration,
+			info:          info,
+			configDir:     o.ConfigDir,
+		})
+		jobsMutex.Unlock()
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed to collect configurations: %w", err)
+	}
+
+	jobsChan := make(chan configJob, len(jobs))
+	errorsChan := make(chan error, o.maxThreads)
+
+	var errors []error
+	var errorMutex sync.Mutex
+	var errorWg sync.WaitGroup
+	errorWg.Add(1)
+
+	go func() {
+		defer errorWg.Done()
+		for err := range errorsChan {
+			errorMutex.Lock()
+			errors = append(errors, err)
+			errorMutex.Unlock()
+		}
+	}()
+
+	var wg sync.WaitGroup
+	var processedCount int64
+	var processedMutex sync.Mutex
+
+	for i := 0; i < o.maxThreads; i++ {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			workerProcessedCount := 0
+			logrus.Infof("Worker %d started", workerID)
+
+			for job := range jobsChan {
+				if err := processConfiguration(job, ocpVersion, workerID, allowedClusterProfiles); err != nil {
+					select {
+					case errorsChan <- err:
+					default:
+						logrus.WithError(err).Errorf("Worker %d failed to process configuration, error channel full", workerID)
+					}
+				}
+
+				workerProcessedCount++
+				processedMutex.Lock()
+				processedCount++
+				currentProcessed := processedCount
+				processedMutex.Unlock()
+
+				if currentProcessed%100 == 0 || currentProcessed == int64(len(jobs)) {
+					logrus.Infof("Progress: %d/%d configurations processed (%.1f%%)",
+						currentProcessed, len(jobs), float64(currentProcessed)/float64(len(jobs))*100)
+				}
+			}
+
+			logrus.Infof("Worker %d finished processing %d configurations", workerID, workerProcessedCount)
+		}(i)
+	}
+
+	logrus.Infof("Processing %d configurations with %d threads", len(jobs), o.maxThreads)
+	for _, job := range jobs {
+		jobsChan <- job
+	}
+	close(jobsChan)
+
+	wg.Wait()
+
+	close(errorsChan)
+	errorWg.Wait()
+
+	errorMutex.Lock()
+	finalErrors := make([]error, len(errors))
+	copy(finalErrors, errors)
+	errorMutex.Unlock()
+
+	successCount := len(jobs) - len(finalErrors)
+	logrus.Infof("Processing completed: %d successful, %d errors out of %d total configurations",
+		successCount, len(finalErrors), len(jobs))
+
+	if len(finalErrors) > 0 {
+		logrus.Errorf("Failed to process %d configurations", len(finalErrors))
+		return utilerrors.NewAggregate(finalErrors)
+	}
+
+	logrus.Info("All configurations processed successfully")
+	return nil
+}
+
+func processConfiguration(job configJob, ocpVersion ocplifecycle.MajorMinor, workerID int, allowedClusterProfiles map[string]bool) error {
+	output := config.DataWithInfo{Configuration: *job.configuration, Info: *job.info}
+
+	configPath := job.info.RelativePath()
+	logger := logrus.WithFields(logrus.Fields{
+		"worker":  workerID,
+		"org":     job.info.Metadata.Org,
+		"repo":    job.info.Metadata.Repo,
+		"branch":  job.info.Metadata.Branch,
+		"config":  configPath,
+		"variant": job.info.Metadata.Variant,
+	})
+
+	logger.Info("Worker processing configuration")
+
+	originalTestCount := len(output.Configuration.Tests)
+	updateIntervalFieldsForMatchedSteps(&output, ocpVersion, allowedClusterProfiles)
+
+	modifiedTests := 0
+	for _, test := range output.Configuration.Tests {
+		if test.Cron != nil || test.Interval != nil {
+			modifiedTests++
+		}
+	}
+
+	if err := output.CommitTo(job.configDir); err != nil {
+		logger.WithError(err).Error("Failed to commit configuration")
+		return fmt.Errorf("failed to commit configuration for %s/%s@%s: %w",
+			job.info.Metadata.Org, job.info.Metadata.Repo, job.info.Metadata.Branch, err)
+	}
+
+	logger.WithFields(logrus.Fields{
+		"total_tests":    originalTestCount,
+		"modified_tests": modifiedTests,
+	}).Info("Worker completed processing configuration")
+	return nil
 }
 
 func updateIntervalFieldsForMatchedSteps(
 	configuration *config.DataWithInfo,
 	version ocplifecycle.MajorMinor,
+	allowedClusterProfiles map[string]bool,
 ) {
 	testVersion, err := ocplifecycle.ParseMajorMinor(extractVersion(configuration.Info.Metadata.Branch))
 	if err != nil {
 		return
 	}
 	if configuration.Info.Metadata.Org == "openshift" || configuration.Info.Metadata.Org == "openshift-priv" {
-		for _, test := range configuration.Configuration.Tests {
+		for i := range configuration.Configuration.Tests {
+			test := &configuration.Configuration.Tests[i]
 			if !strings.Contains(test.As, "mirror-nightly-image") && !strings.Contains(test.As, "promote-") {
+				// Skip tests that don't match the cluster profiles filter
+				if allowedClusterProfiles != nil && !shouldProcessTest(test, allowedClusterProfiles) {
+					continue
+				}
 				if test.Cron != nil {
-					// check if less then past past version
-					if testVersion.Less(ocplifecycle.MajorMinor{Major: version.Major, Minor: version.Minor - 2}) {
-						correctCron, err := isExecutedAtMostXTimesAMonth(*test.Cron, 1)
+					n3Version := ocplifecycle.MajorMinor{Major: version.Major, Minor: version.Minor - 3}
+					if testVersion.Less(n3Version) || *testVersion == n3Version {
+						correctCron, err := isExecutedAtMostOncePerYear(*test.Cron)
 						if err != nil {
 							logrus.Warningf("Can't parse cron string %s", *test.Cron)
 							continue
 						}
 						if !correctCron {
-							*test.Cron = generateMonthlyCron()
+							*test.Cron = generateYearlyCron()
 						}
-					} else if testVersion.GetVersion() == version.GetPastPastVersion() {
+					} else if testVersion.GetVersion() == fmt.Sprintf("%d.%d", version.Major, version.Minor-2) {
 						correctCron, err := isExecutedAtMostXTimesAMonth(*test.Cron, 2)
 						if err != nil {
 							logrus.Warningf("Can't parse cron string %s", *test.Cron)
@@ -114,21 +316,22 @@ func updateIntervalFieldsForMatchedSteps(
 					}
 				}
 				if test.Interval != nil {
-					if testVersion.Less(ocplifecycle.MajorMinor{Major: version.Major, Minor: version.Minor - 2}) {
+					n3Version := ocplifecycle.MajorMinor{Major: version.Major, Minor: version.Minor - 3}
+					if testVersion.Less(n3Version) || *testVersion == n3Version {
 						duration, err := time.ParseDuration(*test.Interval)
 						if err != nil {
-							logrus.Warningf("Can't parse interval string %s", *test.Cron)
+							logrus.Warningf("Can't parse interval string %s", *test.Interval)
 							continue
 						}
-						if duration < time.Hour*24*28 {
-							cronExpr := generateWeeklyWeekendCron()
+						if duration < time.Hour*24*365 {
+							cronExpr := generateYearlyCron()
 							test.Cron = &cronExpr
 							test.Interval = nil
 						}
-					} else if testVersion.GetVersion() == version.GetPastPastVersion() {
+					} else if testVersion.GetVersion() == fmt.Sprintf("%d.%d", version.Major, version.Minor-2) {
 						duration, err := time.ParseDuration(*test.Interval)
 						if err != nil {
-							logrus.Warningf("Can't parse interval string %s", *test.Cron)
+							logrus.Warningf("Can't parse interval string %s", *test.Interval)
 							continue
 						}
 						if duration < time.Hour*24*14 {
@@ -139,7 +342,7 @@ func updateIntervalFieldsForMatchedSteps(
 					} else if testVersion.GetVersion() == version.GetPastVersion() {
 						duration, err := time.ParseDuration(*test.Interval)
 						if err != nil {
-							logrus.Warningf("Can't parse interval string %s", *test.Cron)
+							logrus.Warningf("Can't parse interval string %s", *test.Interval)
 							continue
 						}
 						if duration < time.Hour*24*7 {
@@ -152,6 +355,60 @@ func updateIntervalFieldsForMatchedSteps(
 			}
 		}
 	}
+}
+
+// shouldProcessTest checks if a test should be processed based on cluster profiles filter
+func shouldProcessTest(test *api.TestStepConfiguration, allowedClusterProfiles map[string]bool) bool {
+	clusterProfile := test.GetClusterProfileName()
+
+	// If the test doesn't have a cluster profile, include it
+	if clusterProfile == "" {
+		return true
+	}
+
+	// Check if the cluster profile is in the allowed list
+	return allowedClusterProfiles[clusterProfile]
+}
+
+func isExecutedAtMostOncePerYear(cronExpr string) (bool, error) {
+	switch strings.ToLower(cronExpr) {
+	case "@daily":
+		cronExpr = "0 0 * * *"
+	case "@weekly":
+		cronExpr = "0 0 * * 0"
+	case "@monthly":
+		cronExpr = "0 0 1 * *"
+	case "@yearly", "@annually":
+		cronExpr = "0 0 1 1 *"
+	}
+
+	schedule, err := cron.Parse(cronExpr)
+	if err != nil {
+		return false, err
+	}
+	start := time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC)
+	end := start.AddDate(1, 0, 0)
+
+	executionCount := 0
+	maxIterations := 400
+	iterations := 0
+
+	for {
+		iterations++
+		if iterations > maxIterations {
+			logrus.Warningf("Cron expression '%s' might be invalid, stopping after %d iterations", cronExpr, maxIterations)
+			return false, fmt.Errorf("cron expression '%s' appears to be invalid or causes infinite loop", cronExpr)
+		}
+
+		next := schedule.Next(start)
+		if next.After(end) || next.Equal(end) {
+			break
+		}
+		executionCount++
+		start = next
+	}
+
+	return executionCount <= 1, nil
 }
 
 func isExecutedAtMostXTimesAMonth(cronExpr string, x int) (bool, error) {
@@ -174,7 +431,16 @@ func isExecutedAtMostXTimesAMonth(cronExpr string, x int) (bool, error) {
 	end := start.AddDate(0, 1, 0)
 
 	executionCount := 0
+	maxIterations := 100 // Allow counting up to ~100 executions per month (daily = ~31)
+	iterations := 0
+
 	for {
+		iterations++
+		if iterations > maxIterations {
+			logrus.Warningf("Cron expression '%s' might be invalid, stopping after %d iterations", cronExpr, maxIterations)
+			return false, fmt.Errorf("cron expression '%s' appears to be invalid or causes infinite loop", cronExpr)
+		}
+
 		next := schedule.Next(start)
 		if next.After(end) {
 			break
@@ -198,6 +464,18 @@ func generateBiWeeklyCron() string {
 
 func generateMonthlyCron() string {
 	return fmt.Sprintf("%d %d %d * *", rand.Intn(60), rand.Intn(24), rand.Intn(28)+1)
+}
+
+func generateYearlyCron() string {
+	// Generate a cron that runs once per year on a random day
+	// Format: minute hour day month *
+	// Pick a random month (1-12) and day (1-28 to avoid month boundary issues)
+	month := rand.Intn(12) + 1
+	day := rand.Intn(28) + 1
+	hour := rand.Intn(24)
+	minute := rand.Intn(60)
+
+	return fmt.Sprintf("%d %d %d %d *", minute, hour, day, month)
 }
 
 func extractVersion(s string) string {

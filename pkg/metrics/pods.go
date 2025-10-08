637d2c958bbfce64c27127f6e2f0f96cf3bfb281
Nikolaos Moraitis <nmoraiti@redhat.com>
2025-10-08 18:25:54 +0300

    ensure the cluster profiles in the configuration metrics plus small fixes
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

--- pkg/metrics/pods.go
+++ pkg/metrics/pods.go
@@ -53,60 +53,64 @@ func (p *PodLifecyclePlugin) Name() string {
 }
 
 func (p *PodLifecyclePlugin) Record(ev MetricsEvent) {
+	e, ok := ev.(*PodLifecycleMetricsEvent)
+	if !ok {
+		return
+	}
+
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	if e, ok := ev.(*PodLifecycleMetricsEvent); ok {
-		pod := &corev1.Pod{}
-		if err := p.client.Get(p.ctx, ctrlruntimeclient.ObjectKey{Namespace: e.Namespace, Name: e.PodName}, pod); err != nil {
-			p.logger.WithError(err).Errorf("Failed to get pod %s/%s for lifecycle metrics", e.Namespace, e.PodName)
-			return
-		}
-
-		e.CreationTime = &pod.CreationTimestamp.Time
-		e.StartTime = &pod.Status.StartTime.Time
-		e.CompletionTime = getPodCompletionTime(pod)
-
-		// Only set pod phase if not already set by caller (preserves success/failure determination)
-		if e.PodPhase == "" {
-			e.PodPhase = pod.Status.Phase
-		}
-		e.ConditionTransitionTimes = make(map[string]time.Time)
-		for _, cond := range pod.Status.Conditions {
-			e.ConditionTransitionTimes[string(cond.Type)] = cond.LastTransitionTime.Time
-		}
-
-		if scheduledTime, ok := e.ConditionTransitionTimes[string(corev1.PodScheduled)]; ok && e.CreationTime != nil {
-			d := scheduledTime.Sub(*e.CreationTime)
-			e.SchedulingLatency = &d
-		}
-
-		if scheduledTime, ok := e.ConditionTransitionTimes[string(corev1.PodScheduled)]; ok {
-			if initializedTime, ok := e.ConditionTransitionTimes[string(corev1.PodInitialized)]; ok {
-				d := initializedTime.Sub(scheduledTime)
-				e.InitializationLatency = &d
-			}
-		}
-
-		if readyTime, ok := e.ConditionTransitionTimes[string(corev1.PodReady)]; ok && e.CreationTime != nil {
-			d := readyTime.Sub(*e.CreationTime)
-			e.ReadyLatency = &d
-		}
-
-		if e.CreationTime != nil && e.CompletionTime != nil {
-			d := e.CompletionTime.Sub(*e.CreationTime)
-			e.CompletionLatency = &d
-		}
-
-		for _, status := range pod.Status.InitContainerStatuses {
-			e.InitContainerRestarts += int(status.RestartCount)
-			if status.LastTerminationState.Terminated != nil && status.LastTerminationState.Terminated.ExitCode != 0 {
-				e.InitContainerLastError = status.LastTerminationState.Terminated.Reason
-			}
-		}
-
-		p.logger.WithField("event", e).Debug("Recorded pod lifecycle metrics event")
-		p.events = append(p.events, *e)
+	pod := &corev1.Pod{}
+	if err := p.client.Get(p.ctx, ctrlruntimeclient.ObjectKey{Namespace: e.Namespace, Name: e.PodName}, pod); err != nil {
+		p.logger.WithError(err).Errorf("Failed to get pod %s/%s for lifecycle metrics", e.Namespace, e.PodName)
+		return
 	}
+
+	e.CreationTime = &pod.CreationTimestamp.Time
+	e.StartTime = &pod.Status.StartTime.Time
+	e.CompletionTime = getPodCompletionTime(pod)
+
+	// Only set pod phase if not already set by caller (preserves success/failure determination)
+	if e.PodPhase == "" {
+		e.PodPhase = pod.Status.Phase
+	}
+	e.ConditionTransitionTimes = make(map[string]time.Time)
+	for _, cond := range pod.Status.Conditions {
+		e.ConditionTransitionTimes[string(cond.Type)] = cond.LastTransitionTime.Time
+	}
+
+	if scheduledTime, ok := e.ConditionTransitionTimes[string(corev1.PodScheduled)]; ok && e.CreationTime != nil {
+		d := scheduledTime.Sub(*e.CreationTime)
+		e.SchedulingLatency = &d
+	}
+
+	if scheduledTime, ok := e.ConditionTransitionTimes[string(corev1.PodScheduled)]; ok {
+		if initializedTime, ok := e.ConditionTransitionTimes[string(corev1.PodInitialized)]; ok {
+			d := initializedTime.Sub(scheduledTime)
+			e.InitializationLatency = &d
+		}
+	}
+
+	if readyTime, ok := e.ConditionTransitionTimes[string(corev1.PodReady)]; ok && e.CreationTime != nil {
+		d := readyTime.Sub(*e.CreationTime)
+		e.ReadyLatency = &d
+	}
+
+	if e.CreationTime != nil && e.CompletionTime != nil {
+		d := e.CompletionTime.Sub(*e.CreationTime)
+		e.CompletionLatency = &d
+	}
+
+	for _, status := range pod.Status.InitContainerStatuses {
+		e.InitContainerRestarts += int(status.RestartCount)
+		if status.LastTerminationState.Terminated != nil && status.LastTerminationState.Terminated.ExitCode != 0 {
+			e.InitContainerLastError = status.LastTerminationState.Terminated.Reason
+		}
+	}
+
+	p.logger.WithField("event", e).Debug("Recorded pod lifecycle metrics event")
+	p.events = append(p.events, *e)
+
 }
 
 func (p *PodLifecyclePlugin) Events() []MetricsEvent {

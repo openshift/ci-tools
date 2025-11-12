494106c6a6fce1afe8a39080513204ccec8ef0d3
Marius Cornea <mcornea@redhat.com>
2025-11-12 16:25:20 +0200

    Remove metal-perscale-cpt profile references
    Signed-off-by: Marius Cornea <mcornea@redhat.com>

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1424,7 +1424,6 @@ const (
 	ClusterProfileLibvirtS390xAmd64       ClusterProfile = "libvirt-s390x-amd64"
 	ClusterProfileLibvirtS390xVPN         ClusterProfile = "libvirt-s390x-vpn"
 	ClusterProfileMetalPerfscaleBMCPT     ClusterProfile = "metal-perfscale-cpt"
-	ClusterProfileMetalPerfscaleCPT       ClusterProfile = "metal-perscale-cpt"
 	ClusterProfileMetalPerfscaleJetlag    ClusterProfile = "metal-perfscale-jetlag"
 	ClusterProfileMetalPerfscaleOSP       ClusterProfile = "metal-perfscale-osp"
 	ClusterProfileMetalPerfscaleSelfSched ClusterProfile = "metal-perfscale-selfsched"
@@ -1621,7 +1620,6 @@ func ClusterProfiles() []ClusterProfile {
 		ClusterProfileLibvirtS390xAmd64,
 		ClusterProfileLibvirtS390xVPN,
 		ClusterProfileMetalPerfscaleBMCPT,
-		ClusterProfileMetalPerfscaleCPT,
 		ClusterProfileMetalPerfscaleJetlag,
 		ClusterProfileMetalPerfscaleOSP,
 		ClusterProfileMetalPerfscaleSelfSched,
@@ -1879,8 +1877,6 @@ func (p ClusterProfile) ClusterType() string {
 		return "metal-redhat-gs"
 	case ClusterProfileMetalPerfscaleBMCPT:
 		return "metal-perfscale-cpt"
-	case ClusterProfileMetalPerfscaleCPT:
-		return "metal-perscale-cpt"
 	case ClusterProfileMetalPerfscaleJetlag:
 		return "metal-perfscale-jetlag"
 	case ClusterProfileMetalPerfscaleOSP:
@@ -2178,8 +2174,6 @@ func (p ClusterProfile) LeaseType() string {
 		return "libvirt-s390x-vpn-quota-slice"
 	case ClusterProfileMetalPerfscaleBMCPT:
 		return "metal-perfscale-cpt-quota-slice"
-	case ClusterProfileMetalPerfscaleCPT:
-		return "metal-perscale-cpt-quota-slice"
 	case ClusterProfileMetalPerfscaleJetlag:
 		return "metal-perfscale-jetlag-quota-slice"
 	case ClusterProfileMetalPerfscaleOSP:
